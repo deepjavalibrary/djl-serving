013128946e892f9f94ab65142cf1330814576eb0
sindhuvahini <Revathi@1994>
2021-08-21 04:22:07 +0530

    comments in python files

--- serving/python/protocol/request_handler.py
+++ serving/python/protocol/request_handler.py
@@ -5,6 +5,11 @@
 # or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
 # OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
 # and limitations under the License.
+
+"""
+Request handler for the python server.
+"""
+
 import logging
 import struct
 
@@ -12,6 +17,12 @@ int_size = 4
 
 
 def _retrieve_buffer(conn, length):
+    """
+    Retrieves buffer in the specified length.
+    :param conn: socket connection
+    :param length: length of the data to be read
+    :return: retrieved byter array
+    """
     data = bytearray()
 
     while length > 0:
@@ -27,11 +38,21 @@ def _retrieve_buffer(conn, length):
 
 
 def _retrieve_int(conn):
+    """
+    Retrieves int value.
+    :param conn: socket connection
+    :return: retrieved integer value
+    """
     data = _retrieve_buffer(conn, int_size)
     return struct.unpack("!i", data)[0]
 
 
 def retrieve_request(conn):
+    """
+    Retrieves the request data.
+    :param conn: socket connection
+    :return: request byte array
+    """
     content_len = _retrieve_int(conn)
     content = _retrieve_buffer(conn, content_len)
     return content
