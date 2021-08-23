db1d2cb9b35f5a8b8ba063af4334760a285adc25
sindhuvahini <Revathi@1994>
2021-08-23 21:32:34 +0530

    Created Request type enum

---
+++ serving/src/main/java/ai/djl/serving/pyclient/protocol/RequestType.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package ai.djl.serving.pyclient.protocol;
+
+/**
+ * Represents the type of the request to the python process.
+ */
+public enum RequestType {
+    PREPROCESS(0, "preprocess", "Pre-process"),
+    POSTPROCESS(1, "postprocess", "Post-process");
+
+    private final int reqTypeCode;
+    private final String defaultFuncName;
+    private final String displayStr;
+
+    RequestType(int reqTypeCode, String defaultFuncName, String displayStr) {
+        this.reqTypeCode = reqTypeCode;
+        this.defaultFuncName = defaultFuncName;
+        this.displayStr = displayStr;
+    }
+
+    /**
+     * Gets the function name.
+     *
+     * @return functionName
+     */
+    public String functionName() {
+        return defaultFuncName;
+    }
+
+    /**
+     * Gets the display string of the request type.
+     *
+     * @return displayStr
+     */
+    public String displayStr() {
+        return displayStr;
+    }
+
+    /**
+     * Gets the request type code.
+     *
+     * @return reqTypeCode
+     */
+    public int reqTypeCode() {
+        return reqTypeCode;
+    }
+}
