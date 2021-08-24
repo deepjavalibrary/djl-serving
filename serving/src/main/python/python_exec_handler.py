cad3dba3c979f7cec8b33ce1065d76ef72c04cc3
sindhuvahini <Revathi@1994>
2021-08-25 05:14:59 +0530

    adding license header

--- serving/src/main/python/python_exec_handler.py
+++ serving/src/main/python/python_exec_handler.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+# except in compliance with the License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS"
+# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied. See the License for
+# the specific language governing permissions and limitations under the License.
 from protocol.request import Request
 from util.codec_utils import decode_input
 from util.np_util import np_to_djl_encode, djl_to_np_decode
--- serving/src/main/python/util/np_util.py
+++ serving/src/main/python/util/np_util.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+# except in compliance with the License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS"
+# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied. See the License for
+# the specific language governing permissions and limitations under the License.
 import numpy as np
 
 from util.binary_util import get_long, get_char, set_int, set_str, get_str, get_int, get_byte_as_int, get_bytes, \
