6125fefc4292449e70157140a80582b65b0b4524
sindhuvahini <Revathi@1994>
2021-08-25 00:16:12 +0530

    changes to include input in processor execution

--- serving/src/main/python/python_exec_handler.py
+++ serving/src/main/python/python_exec_handler.py
@@ -1,11 +1,13 @@
+from protocol.output import Output
 from protocol.request import Request
+from util.codec_utils import decode_input
+from util.numpy_djl_util import np_to_djl_encode
 from util.packaging_util import get_class_name
 
 
-def _exec_processor(request: Request):
+def _exec_processor(request: Request, function_param):
     python_file = request.get_python_file()
     function_name = request.get_function_name()
-    function_param = request.get_function_param()
 
     processor_class = get_class_name(python_file, function_name)
     preprocessor = processor_class()
@@ -13,6 +15,9 @@ def _exec_processor(request: Request):
     return data
 
 
-def run_processor(request: Request) -> bytearray:
-    response_data = _exec_processor(request)
-    return response_data
+def run_processor(request: Request) -> Output:
+    input_bytes = request.get_function_param()
+    input = decode_input(input_bytes)
+    np_list = _exec_processor(request, input)
+    djl_bytes = np_to_djl_encode(np_list)
+    return djl_bytes
