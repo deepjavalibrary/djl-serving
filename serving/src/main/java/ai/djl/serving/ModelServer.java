e2e31d16592c3e7a768f75a095ee7e0272427dc9
Frank Liu <frankfliu2000@gmail.com>
2022-05-02 09:48:50 -0700

    [serving] Install engine plugin if needed (#87)

--- serving/src/main/java/ai/djl/serving/ModelServer.java
+++ serving/src/main/java/ai/djl/serving/ModelServer.java
@@ -25,6 +25,7 @@ import ai.djl.repository.Repository;
 import ai.djl.repository.zoo.ModelNotFoundException;
 import ai.djl.serving.http.ServerStartupException;
 import ai.djl.serving.models.ModelManager;
+import ai.djl.serving.plugins.DependencyManager;
 import ai.djl.serving.plugins.FolderScanPluginManager;
 import ai.djl.serving.util.ConfigManager;
 import ai.djl.serving.util.Connector;
@@ -354,10 +355,13 @@ public class ModelServer implements AutoCloseable {
                     engineName = tokens[2].isEmpty() ? null : tokens[2];
                 }
                 if (tokens.length > 3) {
-                    Engine engine =
-                            engineName != null
-                                    ? Engine.getEngine(engineName)
-                                    : Engine.getInstance();
+                    Engine engine;
+                    if (engineName != null) {
+                        DependencyManager.getInstance().installEngine(engineName);
+                        engine = Engine.getInstance();
+                    } else {
+                        engine = Engine.getInstance();
+                    }
                     devices = parseDevices(tokens[3], engine);
                 }
             } else {
