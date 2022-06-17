031e4a5dc672da2a91c7eb7d81f0db1d6ec5ebe8
Qing Lan <qingla@amazon.com>
2022-06-17 10:07:39 -0700

    fix download issue

--- serving/docker/scripts/install_djl_serving.sh
+++ serving/docker/scripts/install_djl_serving.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+DJL_VERSION=$1
 # install Java
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
@@ -6,6 +7,7 @@ fakeroot \
 openjdk-11-jdk-headless \
 curl
 # install DJLServing
+curl https://publish.djl.ai/djl-serving/djl-serving_${DJL_VERSION}-1_all.deb -o djl-serving_all.deb
 dpkg -i djl-serving_all.deb
 rm djl-serving_all.deb
 cp /usr/local/djl-serving-*/conf/log4j2.xml /opt/djl/conf/
