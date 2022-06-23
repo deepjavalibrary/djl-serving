492fee77167021591bd7408981fce93e883e6d4b
sindhuvahinis <somasundaram.sindhu@gmail.com>
2022-06-23 14:20:54 -0700

    Download models

---
+++ tests/integration/download_models.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+platform=$1 # expected values are "cpu" "cpu-full" "pytorch-cu113" "pytorch-inf1" "aarch64"
+
+# all platform models except aarch
+general_platform_models_urls=(
+"https://resources.djl.ai/test-models/pytorch/resnet18_serving_all.zip"
+"https://resources.djl.ai/test-models/tensorflow/resnet50v1.zip"
+"https://resources.djl.ai/test-models/onnxruntime/resnet18-v1-7.zip"
+"https://resources.djl.ai/test-models/mxnet/ssd_resnet50.zip"
+)
+
+# only pytorch models
+aarch_models_urls=(
+"https://resources.djl.ai/test-models/pytorch/resnet18_serving_all.zip"
+)
+
+inf_models_urls=(
+"https://resources.djl.ai/test-models/pytorch/resnet18_inf1.tar.gz"
+)
+
+download()
+{
+  urls=("$@")
+  for url in "${urls[@]}"
+  do
+    filename=${url##*/}
+    # does not download the file, if file already exists
+    if ! [ -f "${filename}" ]; then
+      curl -O "$url"
+    fi
+  done
+}
+
+case $platform in
+cpu|cpu-full|pytorch-cu113)
+  download "${general_platform_models_urls[@]}"
+  ;;
+pytorch-inf1)
+  download "${inf_models_urls[@]}"
+  ;;
+aarch64)
+  download "${aarch_models_urls[@]}"
+  ;;
+*)
+  echo "Bad argument. Expecting one of the values: cpu, cpu-full, pytorch-cu113, pytorch-inf1, aarch64"
+  exit 1;;
+esac
