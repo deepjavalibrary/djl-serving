db0b69848423e250a3287344cf85ce448b5966a0
Frank Liu <frankfliu2000@gmail.com>
2022-06-28 13:15:42 -0700

    [docker] Fix install inferential docker script (#145)

--- serving/docker/scripts/install_inferentia.sh
+++ serving/docker/scripts/install_inferentia.sh
@@ -9,15 +9,22 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends gnupg2
 # install python 3.7 -- required by inferentia
 add-apt-repository -y ppa:deadsnakes/ppa
 
+# remove python 3.8
+apt-get autoremove -y python3
+
 echo "deb https://apt.repos.neuron.amazonaws.com bionic main" > /etc/apt/sources.list.d/neuron.list
 curl -L https://apt.repos.neuron.amazonaws.com/GPG-PUB-KEY-AMAZON-AWS-NEURON.PUB | apt-key add -
 
 # Installing Neuron Tools
 apt-get update
-apt-get install -y aws-neuron-tools python3.7 python3-distutils
+apt-get install -y aws-neuron-tools python3.7 python3.7-distutils
+ln -sf /usr/bin/python3.7 /usr/bin/python3
+ln -sf /usr/bin/python3.7 /usr/bin/python
+
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
 python3 -m pip install -U pip
+rm get-pip.py
 
 # Include torch-neuron
 python3 -m pip install numpy
