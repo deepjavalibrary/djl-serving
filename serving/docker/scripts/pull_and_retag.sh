d12cafa4bf51c29c693fec95e435450d88deb4fb
Qing Lan <qingla@amazon.com>
2022-07-26 17:56:14 -0700

    add docker image migration process

---
+++ serving/docker/scripts/pull_and_retag.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+version=$1
+repo=$2
+images="cpu aarch cpu-full pytorch-inf1 pytorch-cu113 deepspeed"
+
+for image in $images; do
+    if [[ ! "$version" == "nightly" ]]; then
+      if [[ "$image" == "cpu" ]]; then
+        image=$version
+      else
+        image="$version-$image"
+      fi
+    else
+      image="$image-$version"
+    fi
+    docker pull deepjavalibrary/djl-serving:$image
+    docker tag deepjavalibrary/djl-serving:$image $repo/djl-serving:$image
+    docker push $repo/djl-serving:$image
+done
