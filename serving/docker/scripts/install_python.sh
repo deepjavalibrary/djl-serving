640583c44644965e3c940613b84f1536b9279ab3
Qing Lan <qingla@amazon.com>
2022-08-11 13:39:14 -0700

    always install latest

--- serving/docker/scripts/install_python.sh
+++ serving/docker/scripts/install_python.sh
@@ -5,6 +5,6 @@ set -e
 # Ubuntu 20.04 ships python3.8 by default
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -yq python3-dev python3-pip
-python3 -m pip --no-cache-dir install -U pip
+python3 -m pip --no-cache-dir install -U pip --upgrade
 python3 -m pip --no-cache-dir install -U numpy
 ln -s /usr/bin/python3 /usr/bin/python
