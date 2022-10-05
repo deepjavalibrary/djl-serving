bfe80388e470175ed13c23206b81300613fa8a28
Qing Lan <qingla@amazon.com>
2022-10-04 19:30:18 -0700

    minor fix (#285)

--- serving/docker/scripts/security_patch.sh
+++ serving/docker/scripts/security_patch.sh
@@ -5,5 +5,5 @@ IMAGE_NAME=$1
 apt-get update
 
 if [[ "$IMAGE_NAME" == "deepspeed" ]]; then
-  apt-get upgrade dpkg e2fsprogs libdpkg-perl libpcre2-8-0 libpcre3 openssl libsqlite3-0
+  apt-get upgrade -y dpkg e2fsprogs libdpkg-perl libpcre2-8-0 libpcre3 openssl libsqlite3-0
 fi
