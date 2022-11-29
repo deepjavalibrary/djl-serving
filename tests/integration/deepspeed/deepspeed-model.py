0adc89507a6549f89e2a0c57981360d0b5e630dc
Qing Lan <qingla@amazon.com>
2022-11-29 08:54:32 -0800

    add gptj model

--- tests/integration/deepspeed/deepspeed-model.py
+++ tests/integration/deepspeed/deepspeed-model.py
@@ -2,6 +2,8 @@ from djl_python import Input, Output
 import deepspeed
 import torch
 import logging
+import math
+import os
 from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 
 torch.manual_seed(1234)
@@ -10,14 +12,14 @@ def load_model(properties):
     tensor_parallel = properties["tensor_parallel_degree"]
     logging.info(f"Loading model in {properties['model_dir']}")
     model = AutoModelForCausalLM.from_pretrained(properties["model_dir"])
-    tokenizer = AutoTokenizer.from_pretrained(properties["model_dir"], use_fast=False)
+    tokenizer = AutoTokenizer.from_pretrained(properties["model_dir"])
     logging.info(f"Starting DeepSpeed init with TP={tensor_parallel}")
     model = deepspeed.init_inference(model,
                                      mp_size=tensor_parallel,
                                      dtype=model.dtype,
                                      replace_method='auto',
                                      replace_with_kernel_inject=True)
-    return model, tokenizer
+    return model.module, tokenizer
 
 
 def batch_generation(batch_size):
@@ -39,6 +41,25 @@ def batch_generation(batch_size):
 
 model = None
 tokenizer = None
+generator = None
+
+def separate_inference(model, tokenizer, batch_size, length):
+    generate_kwargs = dict(min_length=length, max_new_tokens=length, do_sample=False)
+    input_tokens = tokenizer.batch_encode_plus(batch_generation(batch_size), return_tensors="pt", padding=True)
+    for t in input_tokens:
+        if torch.is_tensor(input_tokens[t]):
+            input_tokens[t] = input_tokens[t].to(torch.cuda.current_device())
+    outputs = model.generate(**input_tokens, **generate_kwargs)
+    return tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
+
+def pipeline_inference(model, tokenizer, batch_size, length):
+    global generator
+    if not generator:
+        local_rank = int(os.getenv('LOCAL_RANK', '0'))
+        generator = pipeline(task='text-generation', model=model, tokenizer=tokenizer, device=local_rank)
+    outputs = generator(batch_generation(batch_size), min_length=length, max_length=length)
+    return [item[0]['generated_text'] for item in outputs]
 
 
 def handle(inputs: Input):
@@ -52,12 +73,9 @@ def handle(inputs: Input):
     data = inputs.get_as_json()
     batch_size = data["batch_size"]
     tokens_to_gen = data["text_length"]
-    generate_kwargs = dict(min_length=tokens_to_gen, max_new_tokens=tokens_to_gen, do_sample=False)
-    input_tokens = tokenizer.batch_encode_plus(batch_generation(batch_size), return_tensors="pt", padding=True)
-    for t in input_tokens:
-        if torch.is_tensor(input_tokens[t]):
-            input_tokens[t] = input_tokens[t].to(torch.cuda.current_device())
-    outputs = model.generate(**input_tokens, **generate_kwargs)
-    outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    if data["use_pipeline"]:
+        outputs = pipeline_inference(model, tokenizer, batch_size, tokens_to_gen)
+    else:
+        outputs = separate_inference(model, tokenizer, batch_size, tokens_to_gen)
     result = {"outputs" : outputs }
     return Output().add_as_json(result)
