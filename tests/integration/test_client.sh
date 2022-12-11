2a321135a73a75dd09f164a66a3da13d201635c7
Frank Liu <frankfliu2000@gmail.com>
2022-12-11 08:42:35 -0800

    [serving] Adds a few more integration tests (#384)
    1. Adds dynamic batching test for Python model
    2. Adds npz/ndlist test fro Python model
    3. Adds PyTorch binary model tests
    4. Adds dynamic batching test for PyTorch model

--- tests/integration/test_client.sh
+++ tests/integration/test_client.sh
@@ -1,36 +1,45 @@
 #!/usr/bin/env bash
 
-set -e
+content_type=$1
+content=$2
+url=$3
 
-url=$1
-content_type=$2
-content=$3
+if [[ -z "$url" ]]; then
+  url="http://127.0.0.1:8080/predictions/test"
+fi
 
 echo "Testing $url with content type: $content_type ..."
 
 if [[ "$content_type" == "tensor/ndlist" ]]; then
   djl-bench ndlist-gen -s $content -o test.ndlist
-  curl -f -X POST $url \
-    -T test.ndlist \
-    -H "Content-type: $content_type"
-  rm -rf test.ndlist
+  curl -sf -m 2 -X POST $url -o out.ndlist -T test.ndlist -H "Content-type: $content_type"
+  ret=$?
+  rm -rf test.ndlist out.ndlist
 elif [[ "$content_type" == "tensor/npz" ]]; then
   djl-bench ndlist-gen -s $content -z -o test.npz
-  curl -f -X POST $url \
-    -T test.npz \
-    -H "Content-type: $content_type"
-  rm -rf test.npz
+  curl -sf -m 2 -X POST $url -o out.npz -T test.npz -H "Content-type: $content_type"
+  ret=$?
+  rm -rf test.npz out.npz
 elif [[ "$content_type" == "text/plain" ]]; then
-  curl -f -X POST $url \
-    -d "$content" \
-    -H "Content-type: $content_type"
+  curl -sf -m 2 -X POST $url -d "$content" -H "Content-type: $content_type"
+  ret=$?
 elif [[ "$content_type" == "image/jpg" ]]; then
-  curl $content -o test.jpg
-  curl -f -X POST $url \
-    -T test.jpg \
-    -H "Content-type: $content_type"
-  rm -rf test.jpg
+  curl -sf -m 2 -X POST $url -T $content -H "Content-type: $content_type"
+  ret=$?
 else
   echo "Content type $content_type not supported!"
   exit 1
 fi
+
+if [[ -z "$EXPECT_TIMEOUT" ]]; then
+  if [[ $ret -ne 0 ]]; then
+    echo "Request failed: $ret"
+    exit 1
+  fi
+else
+  if [[ $ret -ne 28 ]]; then
+    echo "Expecting time out, actual: $ret"
+    exit 1
+  fi
+fi
+
