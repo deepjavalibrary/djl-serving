ac48fc2eaf4ab84c0ea024a02da59a54c67d2b25
Qing Lan <qingla@amazon.com>
2022-12-13 19:55:48 -0600

    use temp directory instead of /tmp (#404)
    * use temp directory
    
    * minor updates
    
    Co-authored-by: Frank Liu <frankfliu2000@gmail.com>

--- engines/python/src/main/java/ai/djl/python/engine/PyModel.java
+++ engines/python/src/main/java/ai/djl/python/engine/PyModel.java
@@ -291,16 +291,17 @@ public class PyModel extends BaseModel {
     }
 
     private void downloadS3(String url) {
-        // TODO: Workaround on SageMaker readonly disk
-        String downloadDir = Utils.getenv("SERVING_DOWNLOAD_DIR", "/tmp");
-        if ("default".equals(downloadDir)) {
-            downloadDir = modelDir.toAbsolutePath().toString();
-        }
         if (pyEnv.getInitParameters().containsKey("model_id")) {
             throw new IllegalArgumentException("model_id and s3url could not both set!");
         }
-        pyEnv.addParameter("model_id", downloadDir);
+        // TODO: Workaround on SageMaker readonly disk
         try {
+            Path tmp = Files.createTempDirectory("download").toAbsolutePath();
+            String downloadDir = Utils.getenv("SERVING_DOWNLOAD_DIR", tmp.toString());
+            if ("default".equals(downloadDir)) {
+                downloadDir = modelDir.toAbsolutePath().toString();
+            }
+            pyEnv.addParameter("model_id", downloadDir);
             String[] commands;
             if (Files.exists(Paths.get("/opt/djl/bin/s5cmd"))) {
                 commands =
