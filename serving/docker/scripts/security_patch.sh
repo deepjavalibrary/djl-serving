4f74ce7360ff86c6ba1578c48f7dc685a856ac62
Sindhu <somasundaram.sindhu@gmail.com>
2023-01-17 08:07:16 -0800

    heimdal 0.20.0-deepspeed

--- serving/docker/scripts/security_patch.sh
+++ serving/docker/scripts/security_patch.sh
@@ -5,9 +5,9 @@ IMAGE_NAME=$1
 apt-get update
 
 if [[ "$IMAGE_NAME" == "deepspeed" ]]; then
-  apt-get upgrade -y dpkg e2fsprogs libdpkg-perl libpcre2-8-0 libpcre3 openssl libsqlite3-0 libdbus-1-3 curl libksba8
+  apt-get upgrade -y dpkg e2fsprogs libdpkg-perl libpcre2-8-0 libpcre3 openssl libsqlite3-0 libdbus-1-3 curl libksba8 libhx509-5-heimdal libkrb5-26-heimdal libasn1-8-heimdal libgssapi3-heimdal
 elif [[ "$IMAGE_NAME" == "pytorch-cu117" ]]; then
   apt-get upgrade -y dpkg e2fsprogs libdpkg-perl libpcre2-8-0 libpcre3 openssl libsqlite3-0 libsepol1 libdbus-1-3 curl
 elif [[ "$IMAGE_NAME" == "cpu" ]] ||  [[ "$IMAGE_NAME" == "pytorch-inf1" ]]; then
-  apt-get upgrade -y libpcre2-8-0 libdbus-1-3 curl
+  apt-get upgrade -y libpcre2-8-0 libdbus-1-3 curl libhx509-5-heimdal libkrb5-26-heimdal libasn1-8-heimdal libgssapi3-heimdal
 fi
