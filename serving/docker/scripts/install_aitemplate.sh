281fcc28a580be257605a78969eba3c5b284b461
Qing Lan <qingla@amazon.com>
2023-02-16 17:42:09 -0800

    add AI template package

---
+++ serving/docker/scripts/install_aitemplate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt-get install -y git
+
+# TODO: change the hack on the code to support A10G
+git clone https://github.com/lanking520/AITemplate --recursive
+cd AITemplate/python
+python setup.py bdist_wheel
+pip install dist/*.whl --force-reinstall
+cd ../../
+rm -rf AITemplate
