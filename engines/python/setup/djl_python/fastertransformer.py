9efb3b72fa3002788167f50b07e61693b43c6a73
rohithkrn <rohith.nallamaddi@gmail.com>
2023-03-03 03:29:46 +0000

    format python code

--- engines/python/setup/djl_python/fastertransformer.py
+++ engines/python/setup/djl_python/fastertransformer.py
@@ -16,13 +16,16 @@ from djl_python import Input, Output
 import logging
 from typing import Optional
 
+
 class FasterTransformerService(object):
     def __init__(self) -> None:
         self.initialized = False
 
     def initialize(self, properties):
-        self.tensor_parallel_degree = int(properties.get("tensor_parallel_degree", 1))
-        self.pipeline_parallel_degree = int(properties.get("pipeline_parallel_degree", 1))
+        self.tensor_parallel_degree = int(
+            properties.get("tensor_parallel_degree", 1))
+        self.pipeline_parallel_degree = int(
+            properties.get("pipeline_parallel_degree", 1))
         self.dtype = properties.get("dtype", "fp32")
         self.model_id = properties.get("model_id")
         self.model = self.load_model()
@@ -31,7 +34,7 @@ class FasterTransformerService(object):
     def load_model(self):
         logging.info(f"Loading model: {self.model_id}")
         return ft.init_inference(self.model_id, self.tensor_parallel_degree,
-                                    self.pipeline_parallel_degree, self.dtype)
+                                 self.pipeline_parallel_degree, self.dtype)
 
     def inference(self, inputs: Input):
         try:
@@ -52,13 +55,14 @@ class FasterTransformerService(object):
 
 _service = FasterTransformerService()
 
+
 def handle(inputs: Input) -> Optional[Output]:
-    
+
     if not _service.initialized:
         _service.initialize(inputs.get_properties())
-    
+
     if inputs.is_empty():
         # Model server makes an empty call to warmup the model on startup
         return None
-    
+
     return _service.inference(inputs)
