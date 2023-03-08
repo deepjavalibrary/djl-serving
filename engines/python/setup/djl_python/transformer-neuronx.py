11f8d6d3b7e40abefc79dd86c3a70d01aade071a
Qing Lan <qingla@amazon.com>
2023-03-07 20:37:58 -0800

    add comments

--- engines/python/setup/djl_python/transformer-neuronx.py
+++ engines/python/setup/djl_python/transformer-neuronx.py
@@ -151,6 +151,7 @@ class TransformerNeuronXService(object):
                     f"{self.batch_size} batch size not equal to {len(input_text)} prompt size"
                 )
             with torch.inference_mode():
+                # inf 2 needs padding
                 input_ids = self.tokenizer.batch_encode_plus(
                     input_text, return_tensors="pt", padding=True)['input_ids']
                 generated_sequence = self.model.sample(
