52331f22d86d10b5bfee1e79a8eb362e32b0160d
Sindhu Somasundaram <56774226+sindhuvahinis@users.noreply.github.com>
2023-03-13 09:54:21 -0700

    AOT Support in FT default handler and test cases (#539)
    FT AOT Handler and tests

--- engines/python/setup/djl_python/fastertransformer.py
+++ engines/python/setup/djl_python/fastertransformer.py
@@ -24,22 +24,27 @@ class FasterTransformerService(object):
         self.tensor_parallel_degree = -1
         self.pipeline_parallel_degree = -1
         self.dtype = None
-        self.model_id = None
+        self.model_id_or_path = None
         self.model = None
 
-    def initialize(self, properties):
+    def inititalize_properties(self, properties):
         self.tensor_parallel_degree = int(
             properties.get("tensor_parallel_degree", 1))
         self.pipeline_parallel_degree = int(
             properties.get("pipeline_parallel_degree", 1))
         self.dtype = properties.get("dtype", "fp32")
-        self.model_id = properties.get("model_id")
+        self.model_id_or_path = properties.get("model_id") or properties.get(
+            "model_dir")
+
+    def initialize(self, properties):
+        self.inititalize_properties(properties)
         self.model = self.load_model()
         self.initialized = True
 
     def load_model(self):
-        logging.info(f"Loading model: {self.model_id}")
-        return ft.init_inference(self.model_id, self.tensor_parallel_degree,
+        logging.info(f"Loading model: {self.model_id_or_path}")
+        return ft.init_inference(self.model_id_or_path,
+                                 self.tensor_parallel_degree,
                                  self.pipeline_parallel_degree, self.dtype)
 
     def inference(self, inputs: Input):
@@ -60,6 +65,16 @@ class FasterTransformerService(object):
 
 
 _service = FasterTransformerService()
+
+
+def partition(inputs: Input):
+    properties = inputs.get_properties()
+    _service.inititalize_properties(properties)
+    ft.save_checkpoint(_service.model_id_or_path,
+                       _service.tensor_parallel_degree,
+                       _service.pipeline_parallel_degree,
+                       properties.get("save_mp_checkpoint_path"),
+                       _service.dtype)
 
 
 def handle(inputs: Input) -> Optional[Output]:
--- tests/integration/llm/prepare.py
+++ tests/integration/llm/prepare.py
@@ -233,6 +233,7 @@ def build_sd_handler_model(model):
     options["option.entryPoint"] = "djl_python.stable-diffusion"
     write_properties(options)
 
+
 def build_ft_handler_model(model):
     if model not in ft_handler_list:
         raise ValueError(
@@ -267,6 +268,18 @@ def build_ft_raw_aot_model(model):
     shutil.copyfile("llm/fastertransformer-model.py", "models/test/model.py")
 
 
+def builder_ft_handler_aot_model(model):
+    if model not in ft_model_list:
+        raise ValueError(
+            f"{model} is not one of the supporting handler {list(ft_model_list.keys())}"
+        )
+    options = ft_model_list[model]
+    options["engine"] = "FasterTransformer"
+    options["entryPoint"] = "djl_python.fastertransformer"
+    options["option.save_mp_checkpoint_path"] = "/opt/ml/model/partition-test"
+    write_properties(options)
+
+
 def build_transformers_neuronx_model(model):
     if model not in transformers_neuronx_model_list.keys():
         raise ValueError(
@@ -298,6 +311,7 @@ supported_handler = {
     'fastertransformer': build_ft_handler_model,
     'fastertransformer_raw': build_ft_raw_model,
     'fastertransformer_raw_aot': build_ft_raw_aot_model,
+    'fastertransformer_handler_aot': builder_ft_handler_aot_model,
     'deepspeed_aot': build_ds_aot_model,
     'transformers_neuronx_raw': build_transformers_neuronx_model,
     'transformers_neuronx': build_transformers_neuronx_handler_model
