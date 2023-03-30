77bf5ad7fefdc73a4c3f92ce45cf3bd4c28caaa4
Siddharth Venkatesan <siddvenk@umich.edu>
2023-03-29 17:05:15 -0700

    Optimize batch inference for text generation (#586)

--- tests/integration/llm/client.py
+++ tests/integration/llm/client.py
@@ -247,7 +247,9 @@ def test_handler(model, model_spec):
             if spec.get("stream_output", False):
                 logging.info(f"res: {res.content}")
                 result = res.content.decode().split("\n")[:-1]
-                assert len(result) <= seq_length, "generated more takens than max_new_tokens"
+                assert len(
+                    result
+                ) <= seq_length, "generated more takens than max_new_tokens"
             else:
                 res = res.json()
                 logging.info(f"res {res}")
