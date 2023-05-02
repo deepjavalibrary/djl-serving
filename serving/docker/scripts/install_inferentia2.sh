bb5d2d3856bd3bac4e7ddfde6309153f590d710b
Qing Lan <qingla@amazon.com>
2023-05-02 09:14:19 -0700

    update

--- serving/docker/scripts/install_inferentia2.sh
+++ serving/docker/scripts/install_inferentia2.sh
@@ -14,9 +14,9 @@ echo "deb https://apt.repos.neuron.amazonaws.com ${VERSION_CODENAME} main" >/etc
 curl -L https://apt.repos.neuron.amazonaws.com/GPG-PUB-KEY-AMAZON-AWS-NEURON.PUB | apt-key add -
 
 # https://awsdocs-neuron.readthedocs-hosted.com/en/latest/release-notes/releasecontent.html#inf2-packages
-apt-get update -y && apt-get install -y linux-headers-$(uname -r) && apt-get install -y aws-neuronx-dkms=2.9.*.* \
-    aws-neuronx-collectives=2.13.*.* \
-    aws-neuronx-runtime-lib=2.13.*.* \
-    aws-neuronx-tools=2.10.*.*
+apt-get update -y && apt-get install -y linux-headers-$(uname -r) && apt-get install -y aws-neuronx-dkms=2.9.* \
+    aws-neuronx-collectives=2.13.* \
+    aws-neuronx-runtime-lib=2.13.* \
+    aws-neuronx-tools=2.10.*
 
 export PATH=/opt/aws/neuron/bin:$PATH
