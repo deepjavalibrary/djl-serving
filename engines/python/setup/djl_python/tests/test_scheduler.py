95fbb3779568322a695684b451149f8f47308b1f
sindhuso <somasundaram.sindhu@gmail.com>
2023-05-22 15:14:47 -0700

    fix testcase

--- engines/python/setup/djl_python/tests/test_scheduler.py
+++ engines/python/setup/djl_python/tests/test_scheduler.py
@@ -1,6 +1,7 @@
 import unittest
-from scheduler.lm_block import HuggingfaceGTP2Block
+from djl_python.scheduler.lm_block import HuggingfaceGTP2Block
 import torch
+
 
 class TestScheduler(unittest.TestCase):
 
@@ -8,9 +9,11 @@ class TestScheduler(unittest.TestCase):
         model_name = "gpt2"
         lm_block = HuggingfaceGTP2Block([model_name], {})
 
-        input0 = [torch.tensor([[40, 2883, 6155, 351, 616, 13779, 3290]]),
-                  torch.arange(7)[None, :],
-                  torch.ones(7, dtype=torch.int64)[None, :]]
+        input0 = [
+            torch.tensor([[40, 2883, 6155, 351, 616, 13779, 3290]]),
+            torch.arange(7)[None, :],
+            torch.ones(7, dtype=torch.int64)[None, :]
+        ]
 
         output0 = lm_block.forward(input0, None)
 
@@ -20,7 +23,8 @@ class TestScheduler(unittest.TestCase):
         past_seq = past_key_values[0][0].shape[-2]
         position_ids = torch.tensor([[past_seq]])
         attention_mask = torch.ones(past_seq + 1, dtype=torch.int64)
-        output1 = lm_block.forward([input_ids, position_ids, attention_mask], past_key_values)
+        output1 = lm_block.forward([input_ids, position_ids, attention_mask],
+                                   past_key_values)
 
 
 if __name__ == '__main__':
