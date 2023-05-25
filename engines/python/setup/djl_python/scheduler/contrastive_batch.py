98ba61a3bc76d06b5493f5be03fce81946e8dd56
KexinFeng <fenkexin@amazon.com>
2023-05-24 22:58:42 -0700

    init_kv_cache_with_batch_input, clean (#743-4)

--- engines/python/setup/djl_python/scheduler/contrastive_batch.py
+++ engines/python/setup/djl_python/scheduler/contrastive_batch.py
@@ -17,25 +17,21 @@ import torch
 
 from djl_python.scheduler.batch import Batch
 
-PAD_TOKEN_ID = 220
-
 
 class ContrastiveBatch(Batch):
 
     def __init__(
-        self,
-        seq_dim_order: List[int],
-        past_output_ids: torch.Tensor = None,
-        past_attention_mask: torch.Tensor = None,
-        past_hidden_states: torch.Tensor = None,
-        logits: torch.Tensor = None,
-        past_key_values: List[torch.Tensor] = None,
+            self,
+            past_output_ids: torch.Tensor = None,
+            past_attention_mask: torch.Tensor = None,
+            past_hidden_states: torch.Tensor = None,
+            logits: torch.Tensor = None,
+            past_key_values: List[torch.Tensor] = None,
     ):
         self.past_hidden_states = past_hidden_states
         self.logits = logits
 
-        super().__init__(seq_dim_order,
-                         past_output_ids=past_output_ids,
+        super().__init__(past_output_ids=past_output_ids,
                          past_attention_mask=past_attention_mask,
                          past_key_values=past_key_values)
 
@@ -43,4 +39,7 @@ class ContrastiveBatch(Batch):
         pass
 
     def trim(self, trim_sequence: int, keep_indices: List[int]):
+        pass
+
+    def nudge_to_squeeze_bubble_padding(self, offsets: torch.Tensor, init_kv_cache_len: int):
         pass
--- engines/python/setup/djl_python/scheduler/utils.py
+++ engines/python/setup/djl_python/scheduler/utils.py
@@ -10,6 +10,7 @@
 # or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS"
 # BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied. See the License for
 # the specific language governing permissions and limitations under the License.
+from typing import Tuple
 
 import torch
 
@@ -67,6 +68,28 @@ def trim_tensor(tensor: torch.Tensor,
         return tensor[keep_indices]
 
 
+def nudge_tensor(tensor: torch.Tensor,
+                 offsets: torch.Tensor,
+                 init_seq_len: int,
+                 seq_order: int):
+    if len(offsets.shape) < 2:
+        offsets = offsets.view(-1, 1)
+
+    if torch.all(offsets == 0) or init_seq_len == 0:
+        return tensor
+
+    tensor_new = tensor.clone()
+    for i in range(offsets.shape[0]):
+        offset = offsets[i].item()
+        if seq_order == 1:
+            tensor_new[i, offset: offset + init_seq_len, ...] = tensor[i, :init_seq_len, ...]
+            tensor_new[i, :offset, ...] = 0
+        elif seq_order == 2:
+            tensor_new[i, :, offset: offset + init_seq_len, ...] = tensor[i, :, :init_seq_len, ...]
+
+    return tensor_new
+
+
 def compute_offsets(input_ids: torch.Tensor,
                     config: SearchConfig) -> torch.Tensor:
     num_batch = input_ids.shape[0]
@@ -110,20 +133,58 @@ def compute_attention_mask(input_ids, config: SearchConfig):
     return attention_mask
 
 
-def compute_position_ids(input_ids: torch.Tensor, offsets: torch.Tensor,
-                         past_seq_len: int, repeat: int):
+def compute_position_ids(batch_size: int, input_seq_len: int, offsets: torch.Tensor, past_seq_len: int,
+                         repeat_offset: int):
+    # past_seq_len: the starting position of the whole batch
+    # repeat_offset: interleave_repeat the offsets to match the batch size of input_ids
     position_range = torch.arange(start=past_seq_len,
-                                  end=past_seq_len + input_ids.shape[-1],
+                                  end=past_seq_len + input_seq_len,
                                   step=1,
                                   dtype=torch.int64).view(1, -1)
 
     position_ids = torch.repeat_interleave(position_range,
                                            dim=0,
-                                           repeats=input_ids.shape[0])
+                                           repeats=batch_size)
 
     position_ids_shifted = position_ids - torch.repeat_interleave(
-        offsets.view(-1, 1), dim=0, repeats=repeat)
+        offsets.view(-1, 1), dim=0, repeats=repeat_offset)
 
     position_ids = torch.maximum(position_ids_shifted,
                                  torch.zeros_like(position_ids_shifted))
     return position_ids
+
+
+def assemble_prefix_kv_cache(input_ids, position_ids, attention_mask, kv_cache: Tuple):
+    if kv_cache is None:
+        return None, position_ids, attention_mask, None
+
+    if kv_cache[0][0].shape[0] > 1:
+        raise Exception(
+            "When kv_cache that precedes the input_ids is provided, the init_forward is restricted"
+            " to process one sequence at a time, which is not padded. This avoids the padding "
+            "bubble between the precedent kv_cache and the input_ids.")
+
+    init_kv_cache_len = kv_cache[0][0].shape[2]
+    batch_size = input_ids.shape[0]
+
+    attention_mask = torch.cat([
+        torch.ones(
+            (batch_size, init_kv_cache_len), dtype=torch.int64), attention_mask
+    ], dim=1)
+    position_ids += init_kv_cache_len
+    dummy_input_ids = torch.full([batch_size, init_kv_cache_len],
+                                 fill_value=0,
+                                 dtype=input_ids.dtype)
+
+    kv_cache_copied = []
+    for k, v in kv_cache:
+        k_copied = torch.repeat_interleave(k,
+                                           dim=0,
+                                           repeats=batch_size)
+        v_copied = torch.repeat_interleave(v,
+                                           dim=0,
+                                           repeats=batch_size)
+        kv_cache_copied.append((k_copied, v_copied))
+    kv_cache = tuple(kv_cache_copied)
+
+    return dummy_input_ids, position_ids, attention_mask, kv_cache
