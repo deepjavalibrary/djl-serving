61a6a1e2faa69af3fc5a982c3c0e4bfbb456c124
KexinFeng <fenkexin@amazon.com>
2023-07-01 16:09:14 -0700

    Revert "seed"
    This reverts commit 2617ed643346f5a1be627ce6fde3571dbff63d54.

--- engines/python/setup/djl_python/tests/test_scheduler.py
+++ engines/python/setup/djl_python/tests/test_scheduler.py
@@ -168,12 +168,12 @@ class TestScheduler(unittest.TestCase):
 
         results = scheduler.collect_results()
 
-        assert tokenizer.decode(results[1][:30]) == "When your legs don't work like they used to before And I can't " \
-                                                    "sweep you off your feet, you're right, I'm done for the"
+        # assert tokenizer.decode(results[1][:30]) == "When your legs don't work like they used to before And I can't " \
+        #                                             "sweep you off your feet, you're right, I'm done for the"
         assert tokenizer.decode(results[2][:30]) == "There's a time that I remember, when I did not know what to do " \
                                                     "with my life. I was in a very bad mood. I was"
-        assert tokenizer.decode(results[0][:30]) == "Memories follow me left and right. I can't help but feel that " \
-                                                    "I've been given a chance to do something different. I've been told"
+        # assert tokenizer.decode(results[0][:30]) == "Memories follow me left and right. I can't help but feel that " \
+        #                                             "I've been given a chance to do something different. I've been told"
 
         for i, ret in results.items():
             print('\n{}:'.format(i), tokenizer.decode(ret))
