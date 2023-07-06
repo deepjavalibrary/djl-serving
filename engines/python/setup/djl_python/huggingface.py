08904168f5f8cc3947c920667e59a2c8ff0a1830
Xin Yang <105740670+xyang16@users.noreply.github.com>
2023-07-06 14:48:47 -0700

    [python] JSON format for rolling batch (#899)
    * [python] JSON format for rolling batch
    
    * Review change
    
    * Update integration test
    
    * Review change
    
    * set content-type for rolling batch
    
    ---------
    
    Co-authored-by: Frank Liu <frankfliu2000@gmail.com>

--- engines/python/setup/djl_python/huggingface.py
+++ engines/python/setup/djl_python/huggingface.py
@@ -173,6 +173,8 @@ class HuggingFaceService(object):
                 self.device = int(os.getenv("LOCAL_RANK", 0))
             _rolling_batch_cls = get_rolling_batch_class_from_str(
                 self.rolling_batch_type, is_mpi, self.model_config)
+
+            # TODO: Allow user to set output formatter
             self.rolling_batch = _rolling_batch_cls(model_id_or_path,
                                                     self.device, properties,
                                                     **kwargs)
@@ -236,11 +238,11 @@ class HuggingFaceService(object):
         if self.rolling_batch_type:
             result = self.rolling_batch.inference(input_data, parameters)
             for i in range(inputs.get_batch_size()):
-                res = result[i]
-                encode(outputs,
-                       res,
-                       accept,
-                       key=inputs.get_content().key_at(i))
+                outputs.add(result[i], key="data", batch_index=i)
+
+            content_type = self.rolling_batch.get_content_type()
+            if content_type:
+                outputs.add_property("content-type", content_type)
 
             return outputs
         elif self.enable_streaming:
