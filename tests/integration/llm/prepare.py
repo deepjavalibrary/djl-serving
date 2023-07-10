4b89d9f9cf2d4c40f50bd7219ee91dab1304eb7b
Aaqib Ansari <maaquib@gmail.com>
2023-07-10 15:27:28 -0700

    Adding gpt-neox-20b-quantized to workflow

--- tests/integration/llm/prepare.py
+++ tests/integration/llm/prepare.py
@@ -369,7 +369,7 @@ lmi_dist_model_list = {
         "option.trust_remote_code": True
     },
     "open-llama-7b": {
-        "option.model_id": "s3://djl-llm/open_llama_7b",
+        "option.model_id": "s3://djl-llm/open-llama-7b",
         "option.task": "text-generation",
         "option.tensor_parallel_degree": 4,
         "option.max_rolling_batch_size": 4
