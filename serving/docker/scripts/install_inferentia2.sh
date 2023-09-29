a68044aa25b2eb1e8be5b66b326a6dcbeccab457
Qing Lan <qingla@amazon.com>
2023-09-29 16:42:45 -0700

    remove header installation

--- serving/docker/scripts/install_inferentia2.sh
+++ serving/docker/scripts/install_inferentia2.sh
@@ -14,7 +14,7 @@ echo "deb https://apt.repos.neuron.amazonaws.com ${VERSION_CODENAME} main" >/etc
 curl -L https://apt.repos.neuron.amazonaws.com/GPG-PUB-KEY-AMAZON-AWS-NEURON.PUB | apt-key add -
 
 # https://awsdocs-neuron.readthedocs-hosted.com/en/latest/release-notes/releasecontent.html#inf2-packages
-apt-get update -y && apt-get install -y linux-headers-$(uname -r) && apt-get install -y aws-neuronx-dkms=2.9.* \
+apt-get update -y && apt-get install -y aws-neuronx-dkms=2.9.* \
     aws-neuronx-collectives=2.13.* \
     aws-neuronx-runtime-lib=2.13.* \
     aws-neuronx-tools=2.10.*
