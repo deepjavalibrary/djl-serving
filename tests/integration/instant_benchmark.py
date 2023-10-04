fc9590d7d46f7b4efaae5ba6d45060d76effb8e3
Qing Lan <qingla@amazon.com>
2023-10-04 10:45:51 -0700

    add benchmark fixes

--- tests/integration/instant_benchmark.py
+++ tests/integration/instant_benchmark.py
@@ -18,6 +18,15 @@ parser.add_argument("--template",
                     required=False,
                     type=str,
                     help="The template json string")
+parser.add_argument("--container",
+                    required=False,
+                    type=str,
+                    help="The container to run the job")
+parser.add_argument("--instance",
+                    required=False,
+                    type=str,
+                    help="The current instance name")
+
 parser.add_argument("--job", required=False, type=str, help="The job string")
 args = parser.parse_args()
 
@@ -55,14 +64,14 @@ def parse_raw_template(url):
             iterator += 1
             while iterator < len(lines) and not is_square_bracket(
                     lines[iterator]):
-                commandline.append(lines[iterator])
+                commandline.append(lines[iterator].replace("\\", " "))
                 iterator += 1
         else:
             iterator += 1
         if name and properties and commandline:
             final_result[name] = {
                 "properties": properties,
-                "awscurl": ' '.join(commandline),
+                "awscurl": ' '.join(commandline).encode().hex(),
                 "requirements": requirements
             }
             name = ''
@@ -84,22 +93,45 @@ def write_model_artifacts(properties, requirements=None):
             f.write('\n'.join(requirements) + '\n')
 
 
-def build_running_script(template, job):
-    template = json.loads(template)
+def machine_translation(machine_name: str):
+    translation = {
+        "inf2.2xlarge": 1,
+        "inf2.8xlarge": 1,
+        "inf2.24xlarge": 6,
+        "inf2.48xlarge": 12,
+        "trn1.2xlarge": 1,
+        "trn1.32xlarge": 16
+    }
+    if machine_name.startswith("inf2") or machine_name.startswith("trn1"):
+        return f"pytorch-inf2-{translation[machine_name]}"
+    else:
+        return "deepspeed"
+
+
+def build_running_script(template, job, instance, container):
+    with open(template) as f:
+        template = json.load(f)
     job_template = template[job]
+    job_template['awscurl'] = bytes.fromhex(job_template['awscurl']).decode("utf-8")
     write_model_artifacts(job_template['properties'],
                           job_template['requirements'])
 
+    command_str = f"./launch_container.sh {container} $PWD/models {machine_translation(instance)}"
+    bash_command = ['echo "Start Launching container..."', command_str, job_template['awscurl']]
+    with open("instant_benchmark.sh", "w") as f:
+        f.write('\n'.join(bash_command))
+
 
 if __name__ == "__main__":
-    if args.parse_template:
-        result = parse_raw_template(args.parse_template)
-        command = f"echo \"::set-output name=jobs::{json.dumps(result.keys())}\""
+    if args.parse:
+        result = parse_raw_template(args.parse)
+        logging.info(f"Parsed running instruction: {result}")
+        command = f"echo \"jobs={json.dumps(json.dumps(list(result.keys())))}\" >> $GITHUB_OUTPUT"
         sp.call(command, shell=True)
-        command = f"echo \"::set-output name=template::{json.dumps(result)}\""
+        command = f"echo \"template={json.dumps(json.dumps(json.dumps(result)))}\" >> $GITHUB_OUTPUT"
         sp.call(command, shell=True)
-    elif args.template and args.job:
-        build_running_script(args.template, args.job)
+    elif args.template and args.job and args.instance and args.container:
+        build_running_script(args.template, args.job, args.instance, args.container)
     else:
         parser.print_help()
         raise ValueError("args not supported")
