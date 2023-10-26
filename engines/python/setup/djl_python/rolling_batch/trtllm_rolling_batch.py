890e5eba09be4030dd876c65bee3e69926a30798
Robert Yang <ydm@amazon.com>
2023-10-26 16:09:24 -0700

    Add rolling batch class

---
+++ engines/python/setup/djl_python/rolling_batch/trtllm_rolling_batch.py
@@ -0,0 +1,75 @@
+import numpy as np
+import tritontoolkit
+from tritontoolkit import triton_pybind
+from transformers import AutoTokenizer
+
+from djl_python.rolling_batch.rolling_batch import RollingBatch, stop_on_any_exception
+
+class TRTLLMRollingBatch(RollingBatch):
+	def __init__(self, model_id_or_path, device, properties, **kwargs):
+		"""
+        Initializes the TRTLLMRollingBatch.
+        :param model_id_or_path: model id or path
+        :param properties: other properties of the model, such as decoder strategy
+        """
+		super().__init__(-1, **kwargs)
+		self.core = tritontoolkit.init_triton(model_id_or_path)
+		self.model = self.core.load_model("tensorrt_llm")
+		self.tokenizer = AutoTokenizer.from_pretrained(model_id_or_path, padding_side="left", revision=kwargs.get('revision', None))
+		self.request_cache = OrderedDict()
+
+	def reset(self):
+		"""
+		Stops all current requests and resets state of rolling batch portion of handler
+		"""
+		for key in self.request_cache.keys():
+            # todo stop the asynchronous inference
+		self.request_cache = OrderedDict()
+		super().reset()
+
+	  def get_sample_payload(self, input_text, parameters):
+        input_tokens = self.tokenizer([input_text])
+        input_ids = input_tokens["input_ids"]
+        input_ids_data = np.array(input_ids, dtype=np.int32)
+        input_lengths_data = np.array([[len(input_ids)]], dtype=np.int32)
+        max_new_tokens = 64
+        if "max_new_tokens" in parameters.keys():
+            max_new_tokens = parameters.get("max_new_tokens")
+        request_output_len_data = np.array([[max_new_tokens]], dtype=np.uint32)
+        streaming_data = np.array([[True]], dtype=bool)
+        payload = {
+            "input_ids": input_ids_data,
+            "input_lengths": input_lengths_data,
+            "request_output_len": request_output_len_data,
+            "streaming": streaming_data,
+        }
+        return payload
+
+	def inference(self, input_data, parameters):
+		batch_size = len(input_data)
+		# add pending requests to active requests list
+		new_requests = self.get_new_requests(input_data, parameters, batch_size)
+		# do we take care of prefill/decode in here or does trtllm take care of that?
+		# register new active requests
+		for request in new_requests:
+			result = self.model.inference_async(self.get_sample_payload(request.input_text, request.parameters))
+			self.request_cache[request_id] = {"response_obj":result, "curr_length":0, "text":"", "finished":False}
+
+		# obtain new tokens in all active requests
+		finished_ids = set()
+		for request in self.active_requests:
+			cached_request = request_cache[request.id]
+			data, complete = cached_request["response_obj"].get_result()
+			output_id = data["output_ids"].squeeze().tolist()
+			output_text = " " + self.tokenizer.decode(output_id)
+			cached_request["curr_length"] += 1
+			cached_request["text"] += token
+			if(complete): # placeholder: "finished"
+				finished_ids.add(request_id)
+				cached_request["finished"] = True
+
+		# remove finished requests
+		for finished_id in finished_ids:
+			del self.request_cache[finished_id]
+
+		return self.postprocess_results()
