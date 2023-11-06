5054214a6e0923c37b6eb861ab32ffd83ff6d72c
Qing Lan <qingla@amazon.com>
2023-11-06 14:44:26 -0800

    fix the bug in the cuda compat settings

--- serving/docker/dockerd-entrypoint-with-cuda-compat.sh
+++ serving/docker/dockerd-entrypoint-with-cuda-compat.sh
@@ -7,12 +7,11 @@ verlte() {
 
 # Follow https://docs.aws.amazon.com/sagemaker/latest/dg/inference-gpu-drivers.html
 if [ -f /usr/local/cuda/compat/libcuda.so.1 ]; then
-    cat /usr/local/cuda/version.txt
     CUDA_COMPAT_MAX_DRIVER_VERSION=$(readlink /usr/local/cuda/compat/libcuda.so.1 |cut -d'.' -f 3-)
     echo "CUDA compat package requires Nvidia driver ⩽${CUDA_COMPAT_MAX_DRIVER_VERSION}"
     NVIDIA_DRIVER_VERSION=$(sed -n 's/^NVRM.*Kernel Module *\([0-9.]*\).*$/\1/p' /proc/driver/nvidia/version 2>/dev/null || true)
     echo "Current installed Nvidia driver version is ${NVIDIA_DRIVER_VERSION}"
-    if [ $(verlte $CUDA_COMPAT_MAX_DRIVER_VERSION $NVIDIA_DRIVER_VERSION) ]; then
+    if [ $(verlte $NVIDIA_DRIVER_VERSION $CUDA_COMPAT_MAX_DRIVER_VERSION) ]; then
         echo "Setup CUDA compatibility libs path to LD_LIBRARY_PATH"
         export LD_LIBRARY_PATH=/usr/local/cuda/compat:$LD_LIBRARY_PATH
         echo $LD_LIBRARY_PATH
