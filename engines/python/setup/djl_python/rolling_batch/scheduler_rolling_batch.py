8dc10184d277a01a37c215612dd21d5639f2e72c
KexinFeng <fenkexin@amazon.com>
2023-11-09 11:01:16 -0800

    fix device and architecture

--- engines/python/setup/djl_python/rolling_batch/scheduler_rolling_batch.py
+++ engines/python/setup/djl_python/rolling_batch/scheduler_rolling_batch.py
@@ -220,10 +220,7 @@ class SchedulerRollingBatch(RollingBatch):
         input_ids = self.tokenizer(input_texts,
                                    return_tensors="pt",
                                    padding=True).input_ids
-        if self.device:
-            input_ids = input_ids.to(self.device)
-        else:
-            input_ids = input_ids.to(self.model.device)
+        input_ids = input_ids.to(self.model.device)
         return input_ids
 
     def _get_prompt_ids(self, prompts):
