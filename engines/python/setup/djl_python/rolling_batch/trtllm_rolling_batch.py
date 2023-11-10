ff0dd765127a09f6442f336915b4e0c61710f46e
Robert Yang <141875536+ydm-amazon@users.noreply.github.com>
2023-11-10 13:37:21 -0800

    Change default top_k, top_p, and temperature parameters

--- engines/python/setup/djl_python/rolling_batch/trtllm_rolling_batch.py
+++ engines/python/setup/djl_python/rolling_batch/trtllm_rolling_batch.py
@@ -48,10 +48,9 @@ class TRTLLMRollingBatch(RollingBatch):
         if "seed" in parameters.keys():
             parameters["random_seed"] = int(parameters.pop("seed"))
         if parameters.pop("do_sample", False):
-            # TODO: Set better default values for do_sample
-            parameters["runtime_top_k"] = parameters.get("runtime_top_k", 2)
-            parameters["temperature"] = parameters.get("temperature", 0.7)
-
+            parameters["runtime_top_k"] = parameters.get("runtime_top_k", 5)
+            parameters["runtime_top_p"] = parameters.get("runtime_top_p", 0.85)
+            parameters["temperature"] = parameters.get("temperature", 0.8)
         parameters["streaming"] = parameters.get("streaming", True)
         return parameters
 
