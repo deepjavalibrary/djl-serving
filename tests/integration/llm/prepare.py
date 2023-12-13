8b5c646e492d1f1d5b117539acbc9d1b6da1587b
rohithkrn <rohith.nallamaddi@gmail.com>
2023-12-13 00:27:19 +0000

    update models for TRT-LLM 0.6.1

--- tests/integration/llm/prepare.py
+++ tests/integration/llm/prepare.py
@@ -626,12 +626,15 @@ trtllm_handler_list = {
         "option.output_formatter": "jsonlines",
     },
     "falcon-7b": {
-        "option.model_id": "s3://djl-llm/triton/falcon-7b-tp1-bs4/",
+        "option.model_id": "s3://djl-llm/triton/0.6.1/falcon-7b-tp1-bs16/",
         "option.tensor_parallel_degree": 1,
+        "option.max_input_len": 1024,
+        "option.max_output_len": 512,
+        "option.max_rolling_batch_size": 16,
         "option.output_formatter": "jsonlines",
     },
     "llama2-7b-smoothquant": {
-        "option.model_id": "s3://djl-llm/llama-2-7b-hf/",
+        "option.model_id": "s3://djl-llm/meta-llama-Llama-2-7b-chat-hf/",
         "option.tensor_parallel_degree": 4,
         "option.quantize": "smoothquant",
         "option.smoothquant_per_token": "True",
