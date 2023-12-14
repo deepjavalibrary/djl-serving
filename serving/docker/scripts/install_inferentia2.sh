6108b5dd2ddd242edab2b21ee7cb22319b1aaaa3
Tyler Osterberg <tylertosterberg@gmail.com>
2023-12-14 06:17:04 -0800

    [DLC][TNX] Freeze all aws-neuronx deps (#1397)

--- serving/docker/scripts/install_inferentia2.sh
+++ serving/docker/scripts/install_inferentia2.sh
@@ -14,9 +14,9 @@ echo "deb https://apt.repos.neuron.amazonaws.com ${VERSION_CODENAME} main" >/etc
 curl -L https://apt.repos.neuron.amazonaws.com/GPG-PUB-KEY-AMAZON-AWS-NEURON.PUB | apt-key add -
 
 # https://awsdocs-neuron.readthedocs-hosted.com/en/latest/release-notes/releasecontent.html#inf2-packages
-apt-get update -y && apt-get install -y aws-neuronx-dkms=2.14.* \
-    aws-neuronx-collectives=2.18.* \
-    aws-neuronx-runtime-lib=2.18.* \
-    aws-neuronx-tools=2.15.*
+apt-get update -y && apt-get install -y aws-neuronx-dkms=2.14.5.0 \
+    aws-neuronx-collectives=2.18.19.0* \
+    aws-neuronx-runtime-lib=2.18.15.0* \
+    aws-neuronx-tools=2.15.4.0
 
 export PATH=/opt/aws/neuron/bin:$PATH
