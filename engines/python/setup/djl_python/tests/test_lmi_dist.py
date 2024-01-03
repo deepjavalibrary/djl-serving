9d07a9b9fd168b83b256a0839a773d2cebcf60c6
KexinFeng <fenkexin@amazon.com>
2024-01-03 13:00:13 -0800

    lmi-dist unit test

---
+++ engines/python/setup/djl_python/tests/test_lmi_dist.py
@@ -0,0 +1,81 @@
+import unittest
+
+import torch
+import os, sys
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+relative_path = "../../"
+new_path = os.path.normpath(os.path.join(script_directory, relative_path))
+sys.path.append(new_path)
+# sys.path.append("/usr/local/lib/python3.9/dist-packages/lmi_dist")
+
+from djl_python.rolling_batch.lmi_dist_rolling_batch import LmiDistRollingBatch
+from djl_python.tests.rolling_batch_test_scripts.generator import Generator, print_rank0
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+global_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class TestSchedulerBloom(unittest.TestCase):
+
+    def test_models(self):
+        model_names = [
+            "TheBloke/Llama-2-7B-Chat-fp16",
+            "TinyLlama/TinyLlama-1.1B-Chat-v0.6",
+            # weight model.layers.0.self_attn.rotary_emb.inv_freq does not exist
+            # "TinyLlama/TinyLlama-1.1B-python-v0.1",
+            "codellama/CodeLlama-7b-hf"
+        ]
+
+        for model_id in model_names:
+            properties = {"mpi_mode": "true",
+                          "tensor_parallel_degree": 1,
+                          "dtype": "fp16",
+                          "max_rolling_batch_size": 28,
+                          "model_loading_timeout": 3600,
+                          "max_rolling_batch_prefill_tokens": 1000,
+                          "paged_attention": "True",
+                          "spec_length": 8}
+
+            # ===================== lmi ============================
+            print("=========== before =========")
+            device = int(os.environ.get("RANK", 0))
+            rolling_batch = LmiDistRollingBatch(model_id, device, properties, draft_model_id_or_path=draft_model_id)
+            rolling_batch.output_formatter = None
+            print("reach here")
+
+            gen = Generator(rolling_batch=rolling_batch)
+
+            print('========== init inference ===========')
+            input_str1 = ["Hello, my name is",  # 6
+                          "The president of the United States is",  # 8
+                          "The capital of France is",  # 6
+                          "The future of AI is"]  # 7
+
+            params1 = [{"max_new_tokens": 100, "do_sample": True, "temperature": 0.001},
+                       {"max_new_tokens": 100, "do_sample": True, "temperature": 0.001},
+                       {"max_new_tokens": 100, "do_sample": True, "temperature": 0.001},
+                       {"max_new_tokens": 100, "do_sample": True, "temperature": 0.001}]
+
+            gen.step(step=10, input_str_delta=input_str1, params_delta=params1)
+
+            for _ in range(1):
+                print('========== inference1 ===========')
+                input_str_delta = ["Hello, my name is Hello, my name is Hello, my name is Hello, my name is",  # 22
+                                   "Hello, my name is Hello, my name is Hello, my name is"]  # 17
+
+                params_delta = [{"max_new_tokens": 100, "do_sample": True, "temperature": 0.001},
+                                {"max_new_tokens": 100, "do_sample": True, "temperature": 0.001}]
+
+                gen.step(step=10, input_str_delta=input_str_delta, params_delta=params_delta)
+
+            print('========== inference_infty ===========')
+            gen.step(step=500)
+            for req_id, out in gen.output_all.items():
+                print_rank0(f"\n====req_id: {req_id}=====\n{gen.input_all[req_id][0] + ''.join(out)}\n")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
