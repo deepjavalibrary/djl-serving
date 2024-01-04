ae9664445612058e2c149ff2766af6b558962b74
KexinFeng <fenkexin@amazon.com>
2024-01-03 19:07:21 -0800

    lmi_dist_path

--- engines/python/setup/djl_python/tests/test_lmi_dist.py
+++ engines/python/setup/djl_python/tests/test_lmi_dist.py
@@ -7,7 +7,7 @@ script_directory = os.path.dirname(os.path.abspath(__file__))
 relative_path = "../../"
 new_path = os.path.normpath(os.path.join(script_directory, relative_path))
 sys.path.append(new_path)
-sys.path.append("/usr/local/lib/python3.9/dist-packages/lmi_dist")
+# sys.path.append("/usr/local/lib/python3.9/dist-packages/lmi_dist")
 
 from djl_python.rolling_batch.lmi_dist_rolling_batch import LmiDistRollingBatch
 from djl_python.tests.rolling_batch_test_scripts.generator import Generator, print_rank0
