ee6e59d4397399650a11c4a8fad6cf4c24c7032b
KexinFeng <fenkexin@amazon.com>
2024-01-04 18:20:31 +0000

    clean up

--- engines/python/setup/djl_python/tests/test_lmi_dist.py
+++ engines/python/setup/djl_python/tests/test_lmi_dist.py
@@ -15,6 +15,13 @@ import unittest
 
 import torch
 import os
+import sys
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+relative_path = "../../"
+new_path = os.path.normpath(os.path.join(script_directory, relative_path))
+sys.path.append(new_path)
+sys.path.append("/usr/local/lib/python3.9/dist-packages/lmi_dist")
 
 from djl_python.rolling_batch.lmi_dist_rolling_batch import LmiDistRollingBatch
 from djl_python.tests.rolling_batch_test_scripts.generator import Generator, print_rank0
