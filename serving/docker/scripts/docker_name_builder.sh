0d1184446d16f4ce8f2dd4b66363b1f25beb0068
Zach Kimberg <kimbergz@amazon.com>
2024-03-03 10:01:53 -0800

    [CI] Integrates temp into build, integration, publishing pipeline
    This PR contains a renovated Actions setup for DJL Serving as promised in
    https://github.com/deepjavalibrary/djl-serving/pull/1264. The major change is to
    create a new nightly orchestration action that will call the build, integration,
    and publish actions. Note that the lmic performance and PySDK integration tests
    were not included and will run as usual.
    
    The build was changed to always push to the temp repo. It does still take a
    nightly/release to determine the version used during build. All of the
    publishing to both docker hub and ECR are done in the publish pipeline. It will
    pull the images from the temp repo and then retag and upload them to both docker
    hub (nightly or release) and ECR.
    
    The integration tests are changed to run off only the temp builds. Each of them
    will pull and use the instances from the temp repo.
    
    In summary, here is a summary of the major workflows you will need:
    
    - Nightly - The nightly action runs almost everything by a cron job like usual.
    It will not publish a bad release failing the tests. The list of actions to
    monitor consists of nightly and the two integration tests not included in it.
    - Release - The release can be done by running the nightly action. Only the
    single action needs to be run and it will complete the full verification up to ECR.
    - Development - For development with integration tests, you should begin by
    making a branch on upstream. Then, run the build for your branch. Once it is
    built, you can run any integration test on your branch as usual. You no longer
    have to worry about conflicts with other developers or polluting the public
    docker hub.

--- serving/docker/scripts/docker_name_builder.sh
+++ serving/docker/scripts/docker_name_builder.sh
@@ -3,8 +3,12 @@
 arch=$1
 version=$2
 
-if [[ -z "$version" ]]; then
+if [[ -z $version || "$version" == "nightly" ]]; then
   image="$arch-nightly"
+elif [[ "$version" == "temp"  ]]; then
+  repo="185921645874.dkr.ecr.us-east-1.amazonaws.com/djl-ci-temp"
+  image="$repo:$arch-$GITHUB_SHA"
+  echo "DJLSERVING_TEMP_REPO=$repo" >> $GITHUB_ENV
 else
   if [[ "$arch" == "cpu" ]]; then
     image=$version
@@ -13,4 +17,4 @@ else
   fi
 fi
 
-echo "DJLSERVING_DOCKER_TAG=$image" >> $GITHUB_ENV
+echo "DJLSERVING_DOCKER=$image" >> $GITHUB_ENV
--- serving/docker/scripts/pull_and_retag.sh
+++ serving/docker/scripts/pull_and_retag.sh
@@ -2,19 +2,22 @@
 
 version=$1
 repo=$2
+mode=$3
 images="cpu aarch64 cpu-full pytorch-inf2 pytorch-gpu deepspeed tensorrt-llm"
 
+temprepo="185921645874.dkr.ecr.us-east-1.amazonaws.com/djl-ci-temp"
+
 for image in $images; do
-    if [[ ! "$version" == "nightly" ]]; then
+    if [[ ! "$mode" == "nightly" ]]; then
       if [[ "$image" == "cpu" ]]; then
-        image=$version
+        tag=$version
       else
-        image="$version-$image"
+        tag="$version-$image"
       fi
     else
-      image="$image-$version"
+      tag="$image-nightly"
     fi
-    docker pull deepjavalibrary/djl-serving:$image
-    docker tag deepjavalibrary/djl-serving:$image $repo/djl-serving:$image
-    docker push $repo/djl-serving:$image
+    docker pull $temprepo:$image-$GITHUB_SHA
+    docker tag $temprepo:$image-$GITHUB_SHA $repo:$tag
+    docker push $repo:$tag
 done
