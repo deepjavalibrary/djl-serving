2e29ab77108fb447b44d040f561f57ded7b82a18
Aaqib <maaquib@gmail.com>
2024-03-12 09:31:12 -0700

    Add pytorch-gpu for security patching (#1621)

--- serving/docker/scripts/security_patch.sh
+++ serving/docker/scripts/security_patch.sh
@@ -4,7 +4,8 @@ IMAGE_NAME=$1
 
 apt-get update
 
-if [[ "$IMAGE_NAME" == "deepspeed" ]]; then
+if [[ "$IMAGE_NAME" == "deepspeed" ]] || \
+   [[ "$IMAGE_NAME" == "pytorch-gpu" ]]; then
   apt-get upgrade -y dpkg openssl curl libssl3
 elif [[ "$IMAGE_NAME" == "cpu" ]]; then
   apt-get upgrade -y libpcre2-8-0 libdbus-1-3 curl
