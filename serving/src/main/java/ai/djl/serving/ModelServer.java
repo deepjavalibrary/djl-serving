3614f3c4f208d319d7eb878e7ce5f3353d48d498
Sindhu Somasundaram <56774226+sindhuvahinis@users.noreply.github.com>
2024-05-02 23:03:54 -0700

    Stop the model server when model download fails (#1842)

--- serving/src/main/java/ai/djl/serving/ModelServer.java
+++ serving/src/main/java/ai/djl/serving/ModelServer.java
@@ -71,6 +71,7 @@ import java.util.Objects;
 import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Matcher;
@@ -205,7 +206,7 @@ public class ModelServer {
 
         try {
             initModelStore();
-        } catch (BadWorkflowException e) {
+        } catch (BadWorkflowException | CompletionException e) {
             throw new ServerStartupException(
                     "Failed to initialize startup models and workflows", e);
         }
@@ -440,27 +441,23 @@ public class ModelServer {
                                 -1);
                 workflow = new Workflow(modelInfo);
             }
-            CompletableFuture<Void> f =
-                    modelManager
-                            .registerWorkflow(workflow)
-                            .exceptionally(
-                                    t -> {
-                                        logger.error("Failed register workflow", t);
-                                        Dimension dim = new Dimension("Model", workflow.getName());
-                                        SERVER_METRIC.info(
-                                                "{}",
-                                                new Metric(
-                                                        "ModelLoadingError", 1, Unit.COUNT, dim));
-                                        // delay 3 seconds, allows REST API to send PING
-                                        // response (health check)
-                                        try {
-                                            Thread.sleep(3000);
-                                        } catch (InterruptedException ignore) {
-                                            // ignore
-                                        }
-                                        stop();
-                                        return null;
-                                    });
+            CompletableFuture<Void> f = modelManager.registerWorkflow(workflow);
+            f.exceptionally(
+                    t -> {
+                        logger.error("Failed register workflow", t);
+                        Dimension dim = new Dimension("Model", workflow.getName());
+                        SERVER_METRIC.info(
+                                "{}", new Metric("ModelLoadingError", 1, Unit.COUNT, dim));
+                        // delay 3 seconds, allows REST API to send PING
+                        // response (health check)
+                        try {
+                            Thread.sleep(3000);
+                        } catch (InterruptedException ignore) {
+                            // ignore
+                        }
+                        stop();
+                        return null;
+                    });
             if (configManager.waitModelLoading()) {
                 f.join();
             }
