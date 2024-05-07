b95975b5a6766c4846a94119dd87e98ed01d261e
Qing Lan <qingla@amazon.com>
2024-05-07 12:36:22 -0700

    fix check style issue

--- wlm/src/test/java/ai/djl/serving/wlm/ModelInfoTest.java
+++ wlm/src/test/java/ai/djl/serving/wlm/ModelInfoTest.java
@@ -39,7 +39,6 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
