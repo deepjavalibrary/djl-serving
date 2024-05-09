7bdb74e61120ac709059e74944afa9565d64097d
Robert Yang <141875536+ydm-amazon@users.noreply.github.com>
2024-05-09 10:46:13 -0700

    Update run_max_num_tokens.py

--- tests/integration/llm/run_max_num_tokens.py
+++ tests/integration/llm/run_max_num_tokens.py
@@ -1,8 +1,11 @@
 import argparse
 import json
+import os
 from tensorrt_llm_toolkit.utils.utils import max_token_finder
 
 if __name__ == '__main__':
+    os.mkdir("max_num_token_results")
+    
     parser = argparse.ArgumentParser(description='Parse inputs to script')
     parser.add_argument('i_model_tp_json', help='Input - JSON containing model and tp pairs')
     args = parser.parse_args()
@@ -10,6 +13,7 @@ if __name__ == '__main__':
     with open(args.i_model_tp_json, "r") as file:
         model_tp_dict = json.load(file)
 
+    log_id = 0
     for model_id, tp_list in model_tp_dict.items():
         if isinstance(tp_list, int):
             tp_list = [tp_list]
@@ -19,5 +23,10 @@ if __name__ == '__main__':
                 "tensor_parallel_degree": tensor_parallel_degree,
             }
             model, tp, max_tokens = max_token_finder(properties)
-            print(f"Summary:\nmodel: {model}\n tp: {tp}\n max_tokens: {max_tokens}")
+            output = f"Summary:\nmodel: {model}\n tp: {tp}\n max_tokens: {max_tokens}"
+            print(output)
             # save files and then upload todo
+            with open("max_num_token_results/" + str(log_id) + "_log.txt", "w") as log_file:
+                log_file.write(output)
+            
+                
