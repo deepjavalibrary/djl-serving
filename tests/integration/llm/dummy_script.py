5c2570bbddbb79a2a4f117517cf4d7527680fc56
Robert Yang <141875536+ydm-amazon@users.noreply.github.com>
2024-05-09 10:47:47 -0700

    Create dummy_script.py

---
+++ tests/integration/llm/dummy_script.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    os.mkdir("max_num_token_results")
+    with open("max_num_token_results/" + str(0) + "_log.txt", "w") as log_file:
+                log_file.write("dummy text")
+    with open("max_num_token_results/" + str(2) + "_log.txt", "w") as log_file:
+                log_file.write("dummy text Q")
