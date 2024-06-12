23b9555f47a16adf634c377d82af27f13869b447
Ethan Zhang <ez3141@gmail.com>
2024-06-12 19:41:51 +0000

    Format

--- plugins/secure-mode/src/test/java/ai/djl/serving/plugins/securemode/SecureModePluginTest.java
+++ plugins/secure-mode/src/test/java/ai/djl/serving/plugins/securemode/SecureModePluginTest.java
@@ -15,11 +15,9 @@ package ai.djl.serving.plugins.securemode;
 import ai.djl.ModelException;
 import ai.djl.util.Utils;
 
-import org.apache.commons.io.FileUtils;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
