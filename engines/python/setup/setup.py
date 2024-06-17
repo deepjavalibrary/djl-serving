83d3e785fef237e04b49206264e94fae20cb38fc
Siddharth Venkatesan <siddhave@amazon.com>
2024-06-17 15:15:17 -0700

    pin numpy to <2 in ci/docker (#2074)

--- engines/python/setup/setup.py
+++ engines/python/setup/setup.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
     requirements = ['psutil', 'packaging', 'wheel']
 
     test_requirements = [
-        'numpy',
+        'numpy<2',
         'requests',
         'Pillow',
         'transformers',
