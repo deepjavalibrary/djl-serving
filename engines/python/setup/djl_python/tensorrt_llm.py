add8c24b7b7202ea3f7031b2bfa74fa8e4194186
Aaqib <maaquib@gmail.com>
2024-06-25 19:18:32 -0400

    [fix] Set tokenizer on output_formatter for TRT-LLM Handlers (#2100)

--- engines/python/setup/djl_python/tensorrt_llm.py
+++ engines/python/setup/djl_python/tensorrt_llm.py
@@ -42,7 +42,8 @@ class TRTLLMService(object):
         self.input_format_configs = InputFormatConfigs(
             is_rolling_batch=True,
             is_adapters_supported=False,
-            output_formatter=self.trt_configs.output_formatter)
+            output_formatter=self.trt_configs.output_formatter,
+            tokenizer=self.rolling_batch.get_tokenizer())
         self.initialized = True
         return
 
