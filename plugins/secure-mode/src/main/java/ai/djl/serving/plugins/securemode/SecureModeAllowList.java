4f53236b1e3131f353b5234eb55c1a7baf5bed15
Ethan Zhang <ez3141@gmail.com>
2024-06-28 19:42:17 +0000

    [secure-mode] Add unit test for props allowlist

--- plugins/secure-mode/src/main/java/ai/djl/serving/plugins/securemode/SecureModeAllowList.java
+++ plugins/secure-mode/src/main/java/ai/djl/serving/plugins/securemode/SecureModeAllowList.java
@@ -1,14 +1,28 @@
+/*
+ * Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
 package ai.djl.serving.plugins.securemode;
 
 import java.util.Set;
 
 /** A class for properties that are allowlisted in Secure Mode. */
-public class SecureModeAllowList {
+final class SecureModeAllowList {
+
     public static final Set<String> PROPERTIES_ALLOWLIST =
             Set.of(
                     "engine",
                     "job_queue_size",
                     "option.entryPoint",
+                    "option.task",
                     "option.model_id",
                     "option.batch_size",
                     "option.tensor_parallel_degree",
@@ -47,4 +61,6 @@ public class SecureModeAllowList {
                     "option.max_lora_rank",
                     "option.lora_extra_vocab_size",
                     "option.max_cpu_loras");
+
+    private SecureModeAllowList() {}
 }
