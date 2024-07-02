310ee7fd7834fef8b2abbb8a281d0a9c1e35a006
Frank Liu <frankfliu2000@gmail.com>
2024-07-02 11:21:50 -0700

    [docker] Fixes docker -SNAPSHOT version build issue

--- serving/docker/scripts/install_djl_serving.sh
+++ serving/docker/scripts/install_djl_serving.sh
@@ -18,22 +18,20 @@ if [ -z "$PYTORCH_JNI" ]; then
     ca-certificates \
     vim
   # add corretto https://docs.aws.amazon.com/corretto/latest/corretto-17-ug/generic-linux-install.html
-  wget -O - https://apt.corretto.aws/corretto.key | gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
-  echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | tee /etc/apt/sources.list.d/corretto.list
-  apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends  -y \
-  java-17-amazon-corretto-jdk
+  wget -O - https://apt.corretto.aws/corretto.key | gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg &&
+    echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | tee /etc/apt/sources.list.d/corretto.list
+  apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
+    java-17-amazon-corretto-jdk
 
   # install DJLServing
   if [ ! -f djl-serving_all.deb ]; then
-    curl https://publish.djl.ai/djl-serving/djl-serving_${DJL_VERSION}-1_all.deb -f -o djl-serving_all.deb
+    curl "https://publish.djl.ai/djl-serving/djl-serving_${DJL_VERSION//-/\~}-1_all.deb" -f -o djl-serving_all.deb
   fi
   dpkg -i djl-serving_all.deb
   rm djl-serving_all.deb
 
   mkdir -p /opt/djl/plugins
 else
-  if [[ ! "$DJL_VERSION" == *SNAPSHOT ]]; then
-    djl-serving -i ai.djl.pytorch:pytorch-jni:${PYTORCH_JNI}-${DJL_VERSION}
-    rm -rf /opt/djl/logs
-  fi
+  djl-serving -i "ai.djl.pytorch:pytorch-jni:${PYTORCH_JNI}-${DJL_VERSION}"
+  rm -rf /opt/djl/logs
 fi
