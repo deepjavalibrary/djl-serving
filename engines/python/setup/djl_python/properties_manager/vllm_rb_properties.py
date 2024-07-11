875c1b2c24be7b1d02e6c3b858de8e24a45ce294
Somasundaram <somasundaram.sindhu@gmail.com>
2024-07-10 17:47:46 -0700

    [python] update max_logprobs default for vllm 0.5.1

--- engines/python/setup/djl_python/properties_manager/vllm_rb_properties.py
+++ engines/python/setup/djl_python/properties_manager/vllm_rb_properties.py
@@ -47,7 +47,7 @@ class VllmRbProperties(Properties):
     # Neuron vLLM properties
     device: Optional[str] = None
     preloaded_model: Optional[Any] = None
-    max_logprobs: Optional[int] = 5
+    max_logprobs: Optional[int] = 20
 
     @field_validator('engine')
     def validate_engine(cls, engine):
