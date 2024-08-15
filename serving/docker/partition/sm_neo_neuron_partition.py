c24d56c243d9fda5966a3c6369d3b2bb0e80dc50
Andrew Song <40076917+a-ys@users.noreply.github.com>
2024-08-15 14:26:14 -0700

    [cherry-pick][0.29.0-dlc] [Neo][Neuron] Various CX improvements for Neo Neuron entrypoint (#2296) (#2317)

--- serving/docker/partition/sm_neo_neuron_partition.py
+++ serving/docker/partition/sm_neo_neuron_partition.py
@@ -16,15 +16,13 @@ import logging
 import os
 from types import SimpleNamespace
 from typing import Final, Optional
-import argparse
 import re
 import shutil
+import json
 
-from optimum.commands.export.neuronx import parse_args_neuronx
-
-from sm_neo_utils import (InputConfiguration, CompilationFatalError,
+from sm_neo_utils import (InputConfiguration, OptimizationFatalError,
                           write_error_to_file, get_neo_env_vars,
-                          get_neo_compiler_flags, load_jumpstart_metadata)
+                          load_jumpstart_metadata)
 from utils import extract_python_jar, load_properties
 from properties_manager import PropertiesManager
 from partition import PartitionService
@@ -127,7 +125,7 @@ class NeoNeuronCacheManager():
         /neuronxcc-2.13.68.0+6dfecc895/<Module folders>
         """
         try:
-            jumpstart_model_id = jumpstart_metadata["model_info"]["model_id"]
+            jumpstart_model_id = jumpstart_metadata["model_info"]["ModelId"]
             jumpstart_model_scope = jumpstart_metadata["script_info"]["Scope"]
         except KeyError as key:
             logging.warning(
@@ -157,12 +155,15 @@ class NeoNeuronPartitionService():
         self.compiler_flags: dict = None
         self.compiler_interface: str = None
 
-        env = get_neo_env_vars()
-        self.NEO_COMPILER_OPTIONS: Final[str] = env[0]
-        self.INPUT_MODEL_DIRECTORY: Final[str] = env[1]
-        self.OUTPUT_MODEL_DIRECTORY: Final[str] = env[2]
-        self.COMPILATION_ERROR_FILE: Final[str] = env[3]
-        self.COMPILER_CACHE_LOCATION: Final[str] = env[4]
+        neo_environ = get_neo_env_vars()
+        self.INPUT_MODEL_DIRECTORY: Final[str] = neo_environ[
+            "SM_NEO_INPUT_MODEL_DIR"]
+        self.OUTPUT_MODEL_DIRECTORY: Final[str] = neo_environ[
+            "SM_NEO_COMPILED_MODEL_DIR"]
+        self.COMPILATION_ERROR_FILE: Final[str] = neo_environ[
+            "SM_NEO_COMPILATION_ERROR_FILE"]
+        self.COMPILER_CACHE_LOCATION: Final[str] = neo_environ[
+            "SM_NEO_CACHE_DIR"]
         # Specific to Neuron compilation
         self.CACHE_JUMPSTART_FORMAT: Final[str] = os.environ.get(
             "SM_CACHE_JUMPSTART_FORMAT", "false")
@@ -174,6 +175,8 @@ class NeoNeuronPartitionService():
         logging.info(
             f"Updating Neuron Persistent Cache directory to: {self.COMPILER_CACHE_LOCATION}"
         )
+        if not os.path.isdir(self.COMPILER_CACHE_LOCATION):
+            raise InputConfiguration("Provided Neo cache directory is invalid")
         os.environ['NEURON_COMPILE_CACHE_URL'] = self.COMPILER_CACHE_LOCATION
 
     def initialize_partition_args_namespace(self):
@@ -193,161 +196,6 @@ class NeoNeuronPartitionService():
         self.args.tensor_parallel_degree = None
         self.args.pipeline_parallel_degree = None
         self.args.quantize = None
-
-    def parse_neo_compiler_flags(self):
-        """
-        Parses the compiler_flags field of Neo CompilerOptions.
-        """
-        if self.compiler_flags:
-            logging.debug(f"Compiler Flags: {self.compiler_flags}")
-            if not isinstance(self.compiler_flags, dict):
-                raise InputConfiguration(
-                    "Invalid compiler flags. Ensure that the input is a valid JSON dictionary."
-                )
-
-            if "compile_only" in self.compiler_flags and self.compiler_flags[
-                    "compile_only"].lower() == "true":
-                logging.info(
-                    "Warning: compile_only flag passed. SafeTensors weights or split model "
-                    "weights must be provided to deploy the model.")
-                self.properties["option.partition_schema"] = "compile_only"
-                del self.compiler_flags["compile_only"]
-
-            if "compiler_interface" in self.compiler_flags:
-                self.compiler_interface = self.compiler_flags[
-                    "compiler_interface"]
-                del self.compiler_flags["compiler_interface"]
-                logging.info(
-                    f"{self.compiler_interface} is set as the compiler interface by "
-                    "Neo CompilerOptions.")
-
-    @staticmethod
-    def convert_tnx_options_to_djl_options(options: dict) -> dict:
-        """
-        Converts Transformers-NeuronX options accepted by Neo to the equivalent option
-        in djl-serving. Only options that have a different name or set of values are converted;
-        the remaining are kept as-is. Supports an additional option "continuous_batching"
-        (equivalently "batch_size_for_shared_caches") to allow users to enable continuous batching.
-
-        :param options: A dictionary containing Transformers-NeuronX options as key-value pairs.
-        :return: returns the modified dictionary
-        """
-        amp_dtype_map = {
-            'f32': 'fp32',
-            'f16': 'fp16',
-            'bf16': 'bf16',
-            's8': 'int8'
-        }
-
-        logging.debug(f"tnx options dict: {options}")
-        if "amp" in options:
-            options["option.dtype"] = amp_dtype_map[options["amp"]]
-            del options["amp"]
-        if "tp_degree" in options:
-            options["option.tensor_parallel_degree"] = options["tp_degree"]
-            del options["tp_degree"]
-        if "continuous_batching" in options or "batch_size_for_shared_caches" in options:
-            max_rolling_batch_size = options[
-                "continuous_batching"] if options.get(
-                    "continuous_batching"
-                ) else options["batch_size_for_shared_caches"]
-            options["option.rolling_batch"] = "auto"
-            options["option.max_rolling_batch_size"] = max_rolling_batch_size
-
-            options.pop("continuous_batching", None)
-            options.pop("batch_size_for_shared_caches", None)
-            # can't set batch_size and max_rolling_batch_size at the same time
-            options.pop("batch_size", None)
-
-        return options
-
-    def construct_properties_manager_from_tnx_options(self):
-        """
-        Factory method used to construct a PropertiesManager from Transformers-NeuronX
-        Neo CompilerOptions
-        """
-        self.args.engine = "Python"
-        # Passing a dummy location because it's expected by PropertiesManager
-        self.args.properties_dir = "/dev/null"
-
-        self.properties["model_dir"] = self.INPUT_MODEL_DIRECTORY
-        self.properties["option.model_loader"] = "tnx"
-        self.properties |= NeoNeuronPartitionService.convert_tnx_options_to_djl_options(
-            self.compiler_flags)
-        logging.debug("Constructing PropertiesManager from TNX "
-                      f"options\nargs:{self.args}\nprops:{self.properties}")
-        self.properties_manager = PropertiesManager(
-            self.args, addl_properties=self.properties)
-
-    @staticmethod
-    def convert_optimum_flags_to_djl_options(
-            flags: argparse.Namespace) -> dict:
-        """
-        This takes a namespace created by parsing Optimum CLI flags and maps the values to
-        djl-serving options.
-
-        :param flags: a mamespace object returned by the Optimum ArgumentParser
-        :return: dictionary containing the converted options
-        """
-        OPTIMUM_TO_DJL_MAP = {
-            "task": "option.task",
-            "trust-remote-code": "option.trust_remote_code",
-            "auto_cast_type": "option.dtype",
-            "num_cores": "option.tensor_parallel_degree",
-            # rolling batch must be true for optimum
-            "batch_size": "option.max_rolling_batch_size",
-            "sequence_length": "option.n_positions"
-        }
-
-        props = {}
-
-        logging.debug(f"optimum flags: {flags}")
-
-        # Iterating through the attributes of the namespace
-        for flag, value in vars(flags).items():
-            if flag == "task" and value == "auto":
-                continue
-
-            if flag in OPTIMUM_TO_DJL_MAP:
-                props[OPTIMUM_TO_DJL_MAP[flag]] = value
-                if flag == "batch_size":
-                    props["option.rolling_batch"] = "auto"
-            elif flag == "01":
-                props["option.neuron_optimize_level"] = 1
-            elif flag == "02":
-                props["option.neuron_optimize_level"] = 2
-            elif flag == "03":
-                props["option.neuron_optimize_level"] = 3
-
-        return props
-
-    def construct_properties_manager_from_optimum_flags(self):
-        """
-        Factory method used to construct a PropertiesManager from Optimum Flags Neo CompilerOptions
-        """
-        self.properties["model_dir"] = self.INPUT_MODEL_DIRECTORY
-        self.properties["option.model_loader"] = "optimum"
-        self.args.engine = "Python"
-        # Passing a dummy location because it's expected by PropertiesManager
-        self.args.properties_dir = "/dev/null"
-
-        # Construct the optimum CLI argparser
-        parser = argparse.ArgumentParser()
-        # This function adds the desired arguments to the parser. Not parsing yet.
-        parse_args_neuronx(parser)
-        # The parser requries the --model param and a trailing output parameter.
-        # Passing the dummy arguments so we can still use the parser.
-        optimum_flags = self.compiler_flags["flags"].split()
-        optimum_flags = ["--model", "/dev/null"] + optimum_flags
-        optimum_flags += ["/dev/null"]
-        flags = parser.parse_args(optimum_flags)
-        self.properties |= NeoNeuronPartitionService.convert_optimum_flags_to_djl_options(
-            flags)
-
-        logging.debug("Constructing PropertiesManager from optimum "
-                      f"options\nargs:{self.args}\nprops:{self.properties}")
-        self.properties_manager = PropertiesManager(
-            self.args, addl_properties=self.properties)
 
     def construct_properties_manager_from_serving_properties(self):
         """
@@ -376,9 +224,7 @@ class NeoNeuronPartitionService():
         try:
             return partition_service.run_partition()
         except Exception as exc:
-            raise CompilationFatalError(
-                f"Encountered an error during Transformers-NeuronX compilation: {exc}"
-            )
+            raise OptimizationFatalError(str(exc))
 
     def write_properties(self) -> str:
         """
@@ -419,7 +265,10 @@ class NeoNeuronPartitionService():
 
     def copy_input_files_to_output(self):
         """
-        Copies inputted files to output so that custom entrypoints or requirements files are preserved.
+        Copies input model files to output directory. Compilation outputs are
+        saved in a subdirectory of the output directory, and the generated
+        serving.properties sets the model location to the subdirectory.
+        This is done so that that custom entrypoints or requirements.txt files are preserved.
 
         TODO: Avoid making redundant copies of model weights.
         """
@@ -430,38 +279,52 @@ class NeoNeuronPartitionService():
         with os.scandir(self.OUTPUT_MODEL_DIRECTORY) as it:
             for entry in it:
                 if os.path.abspath(entry.path) != optimized_model_dir:
-                    shutil.move(entry.path, optimized_model_dir)
+                    if entry.name != "serving.properties":
+                        shutil.move(entry.path, optimized_model_dir)
+
+        # use safetensors index to determine which weights to skip copying
+        ignore_fun = None
+        try:
+            safetensors_index_file = os.path.join(
+                self.INPUT_MODEL_DIRECTORY, "model.safetensors.index.json")
+            # build the set of weight files to ignore
+            weight_files = set()
+            weight_files.add(os.path.basename(safetensors_index_file))
+            with open(safetensors_index_file) as f:
+                index = json.load(f)
+                for k, v in index["weight_map"].items():
+                    weight_files.add(v)
+            # function passed to copytree that ignores all matching weight files in the
+            # root input directory
+            def _ignore_fun(directory, files):
+                ignore = []
+                if os.path.abspath(directory) == os.path.abspath(
+                        self.INPUT_MODEL_DIRECTORY):
+                    ignore = [f for f in files if f in weight_files]
+                return ignore
+
+            ignore_fun = _ignore_fun
+        except OSError:
+            logging.info(
+                "model.safetensors.index.json not found. Full model weights will be copied to output."
+            )
 
         shutil.copytree(self.INPUT_MODEL_DIRECTORY,
                         self.OUTPUT_MODEL_DIRECTORY,
-                        dirs_exist_ok=True)
+                        dirs_exist_ok=True,
+                        ignore=ignore_fun)
         self.write_properties()
 
     def neo_partition(self):
         self.update_neuron_cache_location()
         self.initialize_partition_args_namespace()
-        self.compiler_flags = get_neo_compiler_flags(self.NEO_COMPILER_OPTIONS)
-        self.parse_neo_compiler_flags()
-        if self.compiler_interface == "transformers-neuronx":
-            logging.info(
-                "Reading Neo CompilerOptions in transformers-neuronx format. "
-                "serving.properties will be ignored")
-            self.construct_properties_manager_from_tnx_options()
-            logging.info("Reading Neo CompilerOptions in optimum format. "
-                         "serving.properties will be ignored")
-        elif self.compiler_interface == "optimum":
-            self.construct_properties_manager_from_optimum_flags()
-        elif self.compiler_interface:
-            raise InputConfiguration(
-                f"Invalid compiler interface provided. Got: {self.compiler_interface}"
-            )
-        else:
-            logging.info(
-                "Reading compiler options from provided serving.properties file"
-            )
-            self.construct_properties_manager_from_serving_properties()
+        logging.info(
+            "Reading compiler options from provided serving.properties file")
+        self.construct_properties_manager_from_serving_properties()
         logging.info(f"Model options: {self.properties_manager.properties}")
         partition_output = self.run_partition()
+
+        self.copy_input_files_to_output()
 
         if self.CACHE_JUMPSTART_FORMAT.lower() == 'true':
             logging.info("JumpStart cache environment variable is set")
@@ -475,8 +338,6 @@ class NeoNeuronPartitionService():
                     self.OUTPUT_MODEL_DIRECTORY)
                 cache_manager.create_jumpstart_neuron_cache_in_cache_dir(
                     self.jumpstart_metadata)
-
-        self.copy_input_files_to_output()
 
 
 def main():
