7bdd8eb44d0c88c9db884c7707def3024d4ec196
Robert Yang <141875536+ydm-amazon@users.noreply.github.com>
2024-08-21 17:24:31 -0700

    allow list enable streaming

--- plugins/secure-mode/src/main/java/ai/djl/serving/plugins/securemode/SecureModeAllowList.java
+++ plugins/secure-mode/src/main/java/ai/djl/serving/plugins/securemode/SecureModeAllowList.java
@@ -104,5 +104,7 @@ interface SecureModeAllowList {
                     "option.use_fp8_context_fmha",
                     "option.enable_chunked_prefill",
                     "option.cpu_offload_gb_per_gpu",
-                    "option.enable_prefix_caching");
+                    "option.enable_prefix_caching",
+                    "option.enable_streaming",
+                    "option.tgi_compat");
 }
