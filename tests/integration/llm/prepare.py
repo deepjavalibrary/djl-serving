d76442db561f87b4380c8185472c61f2ea9ac5eb
Siddharth Venkatesan <siddhave@amazon.com>
2024-09-24 16:17:30 -0700

    [ci][lmi] remove output formatter from model configurations, test str… (#2404)

--- tests/integration/llm/prepare.py
+++ tests/integration/llm/prepare.py
@@ -183,7 +183,6 @@ transformers_neuronx_handler_list = {
         "option.rolling_batch": 'auto',
         "option.model_loading_timeout": 2400,
         "option.load_split_model": True,
-        "option.output_formatter": "jsonlines"
     },
     "llama-3-8b-rb-vllm": {
         "option.model_id": "s3://djl-llm/llama-3-8b-hf/",
@@ -192,7 +191,6 @@ transformers_neuronx_handler_list = {
         "option.max_rolling_batch_size": 4,
         "option.rolling_batch": 'vllm',
         "option.model_loading_timeout": 2400,
-        "option.output_formatter": "jsonlines"
     },
     "tiny-llama-rb-vllm": {
         "option.model_id": "s3://djl-llm/tinyllama-1.1b-chat/",
@@ -202,7 +200,6 @@ transformers_neuronx_handler_list = {
         "option.rolling_batch": 'vllm',
         "option.model_loader": 'vllm',
         "option.model_loading_timeout": 1200,
-        "option.output_formatter": "jsonlines"
     },
     "mistral-7b-rb": {
         "option.model_id": "s3://djl-llm/mistral-7b-instruct-v02/",
@@ -220,7 +217,6 @@ transformers_neuronx_handler_list = {
         "option.rolling_batch": "auto",
         "option.max_rolling_batch_size": 1,
         "option.model_loading_timeout": 3600,
-        "option.output_formatter": "jsonlines"
     },
     "llama-speculative-compiled-rb": {
         "option.model_id": "s3://djl-llm/llama-2-13b-hf/",
@@ -234,7 +230,6 @@ transformers_neuronx_handler_list = {
         "option.rolling_batch": "auto",
         "option.max_rolling_batch_size": 1,
         "option.model_loading_timeout": 3600,
-        "option.output_formatter": "jsonlines"
     },
     "tiny-llama-rb-aot": {
         "option.model_id": "s3://djl-llm/tinyllama-1.1b-chat/",
@@ -588,13 +583,11 @@ vllm_model_list = {
         "option.model_id": "s3://djl-llm/llama-2-70b-hf/",
         "option.tensor_parallel_degree": 8,
         "option.max_rolling_batch_size": 32,
-        "option.output_formatter": "jsonlines"
     },
     "mixtral-8x7b": {
         "option.model_id": "s3://djl-llm/mixtral-8x7b/",
         "option.tensor_parallel_degree": 8,
         "option.max_rolling_batch_size": 32,
-        "option.output_formatter": "jsonlines"
     },
     "qwen2-7b-fp8": {
         "option.model_id": "neuralmagic/Qwen2-7B-Instruct-FP8",
@@ -792,7 +785,6 @@ trtllm_handler_list = {
         "option.model_id": "s3://djl-llm/llama-2-13b-hf/",
         "option.tensor_parallel_degree": 4,
         "option.rolling_batch": "trtllm",
-        "option.output_formatter": "jsonlines",
     },
     "llama2-7b-smoothquant": {
         "option.model_id": "s3://djl-llm/meta-llama-Llama-2-7b-chat-hf/",
@@ -801,25 +793,21 @@ trtllm_handler_list = {
         "option.smoothquant_per_token": "True",
         "option.smoothquant_per_channel": "True",
         "option.rolling_batch": "trtllm",
-        "option.output_formatter": "jsonlines",
     },
     "internlm-7b": {
         "option.model_id": "internlm/internlm-7b",
         "option.tensor_parallel_degree": 4,
-        "option.output_formatter": "jsonlines",
         "option.trust_remote_code": True
     },
     "baichuan2-13b": {
         "option.model_id": "s3://djl-llm/baichuan2-13b/",
         "option.tensor_parallel_degree": 4,
         "option.baichuan_model_version": "v2_13b",
-        "option.output_formatter": "jsonlines",
         "option.trust_remote_code": True
     },
     "chatglm3-6b": {
         "option.model_id": "s3://djl-llm/chatglm3-6b/",
         "option.tensor_parallel_degree": 4,
-        "option.output_formatter": "jsonlines",
         "option.trust_remote_code": True,
         "option.chatglm_model_version": "chatglm3"
     },
@@ -827,7 +815,6 @@ trtllm_handler_list = {
         "option.model_id": "s3://djl-llm/mistral-7b/",
         "option.tensor_parallel_degree": 4,
         "option.rolling_batch": "trtllm",
-        "option.output_formatter": "jsonlines"
     },
     "gpt-j-6b": {
         "option.model_id": "s3://djl-llm/gpt-j-6b/",
@@ -836,13 +823,11 @@ trtllm_handler_list = {
         "option.max_output_len": 256,
         "option.max_rolling_batch_size": 16,
         "option.rolling_batch": "auto",
-        "option.output_formatter": "jsonlines"
     },
     "qwen-7b": {
         "option.model_id": "Qwen/Qwen-7B",
         "option.tensor_parallel_degree": 4,
         "option.trust_remote_code": True,
-        "option.output_formatter": "jsonlines"
     },
     "gpt2": {
         "option.model_id": "gpt2",
@@ -850,7 +835,6 @@ trtllm_handler_list = {
         "option.max_rolling_batch_size": 16,
         "option.trust_remote_code": True,
         "option.max_draft_len": 20,
-        "option.output_formatter": "jsonlines"
     },
     "santacoder": {
         "option.model_id": "bigcode/santacoder",
@@ -858,21 +842,18 @@ trtllm_handler_list = {
         "option.max_rolling_batch_size": 16,
         "option.trust_remote_code": True,
         "option.gpt_model_version": "santacoder",
-        "option.output_formatter": "jsonlines"
     },
     "llama2-70b": {
         "option.model_id": "s3://djl-llm/llama-2-70b-hf/",
         "option.tensor_parallel_degree": 8,
         "option.use_custom_all_reduce": True,
         "option.max_rolling_batch_size": 32,
-        "option.output_formatter": "jsonlines"
     },
     "mixtral-8x7b": {
         "option.model_id": "s3://djl-llm/mixtral-8x7b/",
         "option.tensor_parallel_degree": 8,
         "option.use_custom_all_reduce": False,
         "option.max_rolling_batch_size": 32,
-        "option.output_formatter": "jsonlines"
     },
     "llama2-7b-chat": {
         "option.model_id": "s3://djl-llm/meta-llama-Llama-2-7b-chat-hf/",
@@ -946,54 +927,46 @@ trtllm_neo_list = {
         "option.model_id": "s3://djl-llm/llama-3-8b-hf/",
         "option.tensor_parallel_degree": 1,
         "option.rolling_batch": "trtllm",
-        "option.output_formatter": "jsonlines"
     },
     "llama3-8b-tp4-awq": {
         "option.model_id": "s3://djl-llm/llama-3-8b-hf/",
         "option.tensor_parallel_degree": 4,
         "option.rolling_batch": "trtllm",
-        "option.output_formatter": "jsonlines",
         "option.quantize": "awq"
     },
     "llama3-8b-tp4-fp8": {
         "option.model_id": "s3://djl-llm/llama-3-8b-hf/",
         "option.tensor_parallel_degree": 4,
         "option.rolling_batch": "trtllm",
-        "option.output_formatter": "jsonlines",
         "option.quantize": "fp8"
     },
     "llama3-8b-tp4-smoothquant": {
         "option.model_id": "s3://djl-llm/llama-3-8b-hf/",
         "option.tensor_parallel_degree": 4,
         "option.rolling_batch": "trtllm",
-        "option.output_formatter": "jsonlines",
         "option.quantize": "smoothquant"
     },
     "llama3-70b-tp8-fp16": {
         "option.model_id": "s3://djl-llm/llama-3-70b-hf/",
         "option.tensor_parallel_degree": 8,
         "option.rolling_batch": "trtllm",
-        "option.output_formatter": "jsonlines"
     },
     "llama3-70b-tp8-awq": {
         "option.model_id": "s3://djl-llm/llama-3-70b-hf/",
         "option.tensor_parallel_degree": 8,
         "option.rolling_batch": "trtllm",
-        "option.output_formatter": "jsonlines",
         "option.quantize": "awq"
     },
     "llama3-70b-tp8-fp8": {
         "option.model_id": "s3://djl-llm/llama-3-70b-hf/",
         "option.tensor_parallel_degree": 8,
         "option.rolling_batch": "trtllm",
-        "option.output_formatter": "jsonlines",
         "option.quantize": "fp8"
     },
     "llama3-70b-tp8-smoothquant": {
         "option.model_id": "s3://djl-llm/llama-3-70b-hf/",
         "option.tensor_parallel_degree": 8,
         "option.rolling_batch": "trtllm",
-        "option.output_formatter": "jsonlines",
         "option.quantize": "smoothquant"
     }
 }
@@ -1211,7 +1184,6 @@ def build_lmi_dist_model(model):
     options = lmi_dist_model_list[model]
     options["engine"] = "MPI"
     options["option.rolling_batch"] = "lmi-dist"
-    options["option.output_formatter"] = "jsonlines"
 
     adapter_ids = options.pop("adapter_ids", [])
     adapter_names = options.pop("adapter_names", [])
@@ -1229,7 +1201,6 @@ def build_vllm_model(model):
     options = vllm_model_list[model]
     options["engine"] = "Python"
     options["option.rolling_batch"] = "vllm"
-    options["option.output_formatter"] = "jsonlines"
 
     adapter_ids = options.pop("adapter_ids", [])
     adapter_names = options.pop("adapter_names", [])
@@ -1258,7 +1229,6 @@ def build_lmi_dist_aiccl_model(model):
     options["option.task"] = "text-generation"
     options["option.tensor_parallel_degree"] = 8
     options["option.rolling_batch"] = "lmi-dist"
-    options["option.output_formatter"] = "jsonlines"
     options["option.max_rolling_batch_size"] = 16
     write_model_artifacts(options)
 
@@ -1301,7 +1271,6 @@ def build_correctness_model(model):
             f"{model} is not one of the supporting handler {list(correctness_model_list.keys())}"
         )
     options = correctness_model_list[model]
-    options["option.output_formatter"] = "json"
     write_model_artifacts(options)
 
 
