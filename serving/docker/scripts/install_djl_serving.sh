3543cf4421e2cddeba2b6d88719a5b0e109b26eb
Siddharth Venkatesan <siddhave@amazon.com>
2024-11-19 10:44:04 -0800

    [docker] refactor install_djl_serving script so that it only needs to be run once

--- serving/docker/scripts/install_djl_serving.sh
+++ serving/docker/scripts/install_djl_serving.sh
@@ -6,34 +6,33 @@ DJL_VERSION=$1
 DJL_SERVING_VERSION=$2
 PYTORCH_JNI=$3
 
-if [ -z "$PYTORCH_JNI" ]; then
-  # install Java
-  apt-get update
-  DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
-    fakeroot \
-    wget \
-    gpg \
-    curl \
-    jq \
-    unzip \
-    ca-certificates \
-    fontconfig \
-    vim
+# install Java
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
+  fakeroot \
+  wget \
+  gpg \
+  curl \
+  jq \
+  unzip \
+  ca-certificates \
+  fontconfig \
+  vim
 # add corretto https://docs.aws.amazon.com/corretto/latest/corretto-17-ug/generic-linux-install.html
-  wget -O - https://apt.corretto.aws/corretto.key | gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg &&
-    echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | tee /etc/apt/sources.list.d/corretto.list
-  apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
-    java-17-amazon-corretto-jdk
+wget -O - https://apt.corretto.aws/corretto.key | gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg &&
+  echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | tee /etc/apt/sources.list.d/corretto.list
+apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
+  java-17-amazon-corretto-jdk
 
-  # install DJLServing package 
-  if [ ! -f djl-serving_all.deb ]; then
-    curl "https://publish.djl.ai/djl-serving/djl-serving_${DJL_SERVING_VERSION//-/\~}-1_all.deb" -f -o djl-serving_all.deb
-  fi
-  dpkg -i djl-serving_all.deb
-  rm djl-serving_all.deb
+# install DJLServing package
+if [ ! -f djl-serving_all.deb ]; then
+  curl "https://publish.djl.ai/djl-serving/djl-serving_${DJL_SERVING_VERSION//-/\~}-1_all.deb" -f -o djl-serving_all.deb
+fi
+dpkg -i djl-serving_all.deb
+rm djl-serving_all.deb
 
-  mkdir -p /opt/djl/plugins
-else
+mkdir -p /opt/djl/plugins
+if [ -n "$PYTORCH_JNI"]; then
   djl-serving -i "ai.djl.pytorch:pytorch-jni:${PYTORCH_JNI}-${DJL_VERSION}"
   rm -rf /opt/djl/logs
 fi
