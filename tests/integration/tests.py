8d43fb3979f881ba09a37f184d4250eb01ccf58d
Shen Teng <sheteng@amazon.com>
2024-12-06 02:08:34 +0000

    add call integration workflow

--- tests/integration/tests.py
+++ tests/integration/tests.py
@@ -11,7 +11,6 @@ import test_client
 
 djl_version = os.environ.get('TEST_DJL_VERSION', '').strip()
 
-
 def is_applicable_cuda_capability(arch: int) -> bool:
     import torch
     if not torch.cuda.is_available():
