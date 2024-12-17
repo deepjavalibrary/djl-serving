468a260e6589a498c7d80be19c38eadf3e7f6c8e
Shen Teng <sheteng@amazon.com>
2024-12-17 21:46:32 +0000

    improve scripts

--- serving/docker/scripts/push_image_from_ECR.sh
+++ serving/docker/scripts/push_image_from_ECR.sh
@@ -1,24 +1,37 @@
 #!/usr/bin/env bash
 # for docker_publish.yml
-version=$1
-repo=$2
-mode=$3
-images="cpu aarch64 cpu-full pytorch-inf2 pytorch-gpu lmi tensor rt-llm"
 
-temprepo="185921645874.dkr.ecr.us-east-1.amazonaws.com/djl-ci-temp"
+set -euo pipefail
+# Validate required arguments
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 <version> <to_repo> <mode> [commit_sha]" >&2
+    exit 1
+fi
+
+version=$1
+to_repo=$2
+mode=$3
+commit_sha=${4:-$GITHUB_SHA}  # Use parameter expansion for default value
+
+images=(cpu aarch64 cpu-full pytorch-inf2 pytorch-gpu lmi tensorrt-llm)
+
+from_repo=$AWS_TMP_ECR_REPO
 
 for image in $images; do
-  if [[ ! "$mode" == "nightly" ]]; then
+
+  if [[ "$mode" == "release" ]]; then
     if [[ "$image" == "cpu" ]]; then
       tag=$version
     else
       tag="$version-$image"
     fi
-  else
+  fi
+
+  if [[ "$mode" == "nightly" ]]; then
     tag="$image-nightly"
   fi
-  echo docker pull $temprepo:$image-$mode-$GITHUB_SHA
-  docker pull $temprepo:$image-$mode-$GITHUB_SHA
-  echo docker tag $temprepo:$image-$mode-$GITHUB_SHA $repo:$tag
-  echo docker push $repo:$tag
+  echo docker pull $from_repo:$image-$mode-$commit_sha
+  docker pull $from_repo:$image-$mode-$commit_sha
+  echo docker tag $from_repo:$image-$mode-$commit_sha $to_repo:$tag
+  echo docker push $to_repo:$tag
 done
