36eaa27d3dc2a0f04a26c4c730ffb61451c8ddba
Sindhu Somasundaram <56774226+sindhuvahinis@users.noreply.github.com>
2025-01-14 10:22:30 -0800

    [docker] bump neuron sdk to 2.21 (#2656)

--- serving/docker/scripts/install_inferentia2.sh
+++ serving/docker/scripts/install_inferentia2.sh
@@ -16,12 +16,12 @@ echo "deb https://apt.repos.neuron.amazonaws.com ${VERSION_CODENAME} main" >/etc
 curl -L https://apt.repos.neuron.amazonaws.com/GPG-PUB-KEY-AMAZON-AWS-NEURON.PUB | apt-key add -
 
 # https://awsdocs-neuron.readthedocs-hosted.com/en/latest/release-notes/releasecontent.html#inf2-packages
-apt-get update -y && apt-get install -y aws-neuronx-collectives=2.22.26.0* \
-  aws-neuronx-runtime-lib=2.22.14.0* \
-  aws-neuronx-tools=2.19.0.0
+apt-get update -y && apt-get install -y aws-neuronx-collectives=2.23.133.0* \
+  aws-neuronx-runtime-lib=2.23.110.0* \
+  aws-neuronx-tools=2.20.204.0
 
 # TODO: Remove this hack after aws-neuronx-dkms install no longer throws an error, this bypasses the `set -ex`
 #       exit criteria. The package is installed and functional after running, just throws an error on install.
-apt-get install -y aws-neuronx-dkms=2.18.12.0 || echo "Installed aws-neuronx-dkms with errors"
+apt-get install -y aws-neuronx-dkms=2.19.64.0 || echo "Installed aws-neuronx-dkms with errors"
 
 export PATH=/opt/aws/neuron/bin:$PATH
