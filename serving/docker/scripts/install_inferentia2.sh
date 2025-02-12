18b395689859309f707a9aaa2cb1a5d53e30d584
Somasundaram <somasundaram.sindhu@gmail.com>
2025-02-12 11:35:55 -0800

    [docker] Bump up neuron to 2.21.1

--- serving/docker/scripts/install_inferentia2.sh
+++ serving/docker/scripts/install_inferentia2.sh
@@ -16,8 +16,8 @@ echo "deb https://apt.repos.neuron.amazonaws.com ${VERSION_CODENAME} main" >/etc
 curl -L https://apt.repos.neuron.amazonaws.com/GPG-PUB-KEY-AMAZON-AWS-NEURON.PUB | apt-key add -
 
 # https://awsdocs-neuron.readthedocs-hosted.com/en/latest/release-notes/releasecontent.html#inf2-packages
-apt-get update -y && apt-get install -y aws-neuronx-collectives=2.23.133.0* \
-  aws-neuronx-runtime-lib=2.23.110.0* \
+apt-get update -y && apt-get install -y aws-neuronx-collectives=2.23.135.0* \
+  aws-neuronx-runtime-lib=2.23.112.0* \
   aws-neuronx-tools=2.20.204.0
 
 # TODO: Remove this hack after aws-neuronx-dkms install no longer throws an error, this bypasses the `set -ex`
