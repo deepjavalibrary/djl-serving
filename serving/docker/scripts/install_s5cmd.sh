ae85fc215151aad7a4c1da79fc099132f649ed39
Siddharth Venkatesan <siddhave@amazon.com>
2025-04-14 12:44:49 -0700

    [lmi][docker] update python dependency versions for lmi container (#2803)

--- serving/docker/scripts/install_s5cmd.sh
+++ serving/docker/scripts/install_s5cmd.sh
@@ -5,9 +5,9 @@ set -ex
 ARCH=$1
 
 if [[ $ARCH == "aarch64" ]]; then
-  curl https://github.com/peak/s5cmd/releases/download/v2.0.0/s5cmd_2.0.0_Linux-arm64.tar.gz -L -o s5cmd.tar.gz
+  curl https://github.com/peak/s5cmd/releases/download/v2.3.0/s5cmd_2.3.0_Linux-arm64.tar.gz -L -o s5cmd.tar.gz
 else
-  curl https://github.com/peak/s5cmd/releases/download/v2.0.0/s5cmd_2.0.0_Linux-64bit.tar.gz -L -o s5cmd.tar.gz
+  curl https://github.com/peak/s5cmd/releases/download/v2.3.0/s5cmd_2.3.0_Linux-64bit.tar.gz -L -o s5cmd.tar.gz
 fi
 
 INSTALL_DIR="/opt/djl/bin"
