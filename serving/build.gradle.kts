9f0970cb6068583632a7524d9231733c7cd5c67b
Raymond Liu <raymond.kh.liu@gmail.com>
2025-05-29 22:14:22 -0700

    Upgrade gradle to 8.14

--- serving/build.gradle.kts
+++ serving/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     ai.djl.javaProject
     ai.djl.publish
     application
-    id("com.netflix.nebula.ospackage") version "11.11.1"
+    id("com.netflix.nebula.ospackage") version "11.11.2"
 }
 
 dependencies {
